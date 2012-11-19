2ebb86e081ad11ffd3a7097d0f1bbaaf9caa4902
Ian McCulloch <ianmcc@physics.uq.edu.au>
2012-11-19 12:10:07 +0000

    Added program_options interface to mp-iexpectation

--- mp/mp-iexpectation3.cpp
+++ mp/mp-iexpectation3.cpp
@@ -10,6 +10,9 @@
 #include "mp-algorithms/arnoldi.h"
 #include "common/polynomial.h"
 #include "tensor/tensor_eigen.h"
+#include "common/prog_opt_accum.h"
+#include "mp/copyright.h"
+#include "common/prog_options.h"
 
 #include "models/spin.h"
 #include "models/spin-u1.h"
@@ -23,6 +26,8 @@
 #include "models/bosehubbard-spinless.h"
 #include "models/bosehubbard-spinless-u1.h"
 #include "models/hubbard-u1su2.h"
+
+namespace prog_opt = boost::program_options;
 
 // binomial coefficient n choose k
 long Binomial(int n, int k)
@@ -666,12 +671,8 @@ TriangularOperator ConstructSpinOperator()
 
 
 
-TriangularOperator ConstructTriOperator()
+TriangularOperator ConstructTriOperator(double t, double tc, double U)
 {
-   double t = 1;
-   double tc = 0.25;
-   double U = 1.0;
-
    SiteBlock Site = CreateSU2HubbardSite();
    double tSqrt2 = (-sqrt(2.0)) * t;  // the -sqrt(2) is an SU(2) factor
    double tcSqrt2 = (-sqrt(2.0)) * tc;  // the -sqrt(2) is an SU(2) factor
@@ -701,62 +702,125 @@ TriangularOperator ConstructTriOperator()
 
 int main(int argc, char** argv)
 {
-   if (argc < 2 || argc > 2)
+   std::string FName;
+   std::string Operator;
+
+   double t = 1;
+   double tc = 0.25;
+   double U = 0;
+   int Power = 1;
+   bool Verbose = false;
+
+   std::cout.precision(getenv_or_default("MP_PRECISION", 14));
+ 
+   try
    {
-      std::cout << "usage: mp-iexpectation <psi>\n";
+      prog_opt::options_description desc("Allowed options", terminal::columns());
+      desc.add_options()
+         ("help", "show this help message")
+	 ("t", prog_opt::value(&t),
+	  FormatDefault("nearest-neighbor hopping (for hubbard etc)", t).c_str())
+	 ("tc", prog_opt::value(&tc),
+	  FormatDefault("cluster hopping (for triangular cluster)", tc).c_str())
+	 ("U", prog_opt::value(&U),
+	  FormatDefault("coulomb repulsion", U).c_str())
+	 ("power", prog_opt::value(&Power),
+	  FormatDefault("Calculate expectation value of operator to this power", Power).c_str())
+         ("verbose,v", prog_opt::bool_switch(&Verbose),
+          "extra debug output")
+	 ;
+
+      prog_opt::options_description hidden("Hidden options");
+      hidden.add_options()
+         ("psi", prog_opt::value<std::string>(&FName), "wavefunction")
+         ("operator", prog_opt::value<std::string>(&Operator), "operator")
+         ;
+
+      prog_opt::positional_options_description p;
+      p.add("psi", 1);
+      p.add("operator", 1);
+
+      prog_opt::options_description opt;
+      opt.add(desc).add(hidden);
+
+      prog_opt::variables_map vm;        
+      prog_opt::store(prog_opt::command_line_parser(argc, argv).
+                      options(opt).positional(p).run(), vm);
+      prog_opt::notify(vm);    
+
+      if (vm.count("help") > 0 || vm.count("operator") == 0)
+      {
+         print_copyright(std::cerr);
+         std::cerr << "usage: mp-expectation3 <psi1> <operator>\n";
+         std::cerr << desc << '\n';
+         return 1;
+      }
+      
+      long CacheSize = getenv_or_default("MP_CACHESIZE", 655360);
+      pvalue_ptr<InfiniteWavefunction> PsiPtr = pheap::OpenPersistent(FName, CacheSize, true);
+      InfiniteWavefunction Psi = *PsiPtr;
+      int UnitCellSize = Psi.Psi.size();
+
+      TriangularOperator Op;
+
+      if (Operator == "tri")
+	 Op = ConstructTriOperator(t, tc, U);
+      else
+      {
+	 std::cerr << "Unknown operator!\n";
+	 exit(1);
+      }
+
+      // construct the operator to the given power
+      TriangularOperator Temp = Op;
+      while (Power > 1)
+      {
+	 Op = Op * Temp;
+	 --Power;
+      }
+
+      // Make a LinearWavefunction in the symmetric orthogonality constraint
+      MatrixOperator Identity = MatrixOperator::make_identity(Psi.Psi.Basis1());
+      MatrixOperator Rho = scalar_prod(Psi.C_right, herm(Psi.C_right));
+      LinearWavefunction Phi = Psi.Psi; // no need to bugger around with C_old,C_right
+ 
+      MatrixOperator LambdaSqrt = SqrtDiagonal(Psi.C_old);
+      MatrixOperator LambdaInvSqrt = InvertDiagonal(LambdaSqrt, InverseTol);
+
+      Phi.set_front(prod(LambdaInvSqrt, Phi.get_front()));
+      Phi.set_back(prod(Phi.get_back(), delta_shift(LambdaSqrt, adjoint(Psi.QShift))));
+      Rho = Psi.C_old;
+      Identity = Rho;
+
+      // Rho and Identity are the same matrix in this representation
+
+      //   TRACE(norm_frob(operator_prod(herm(Phi), Identity, Phi) - Identity));
+      //   TRACE(norm_frob(operator_prod(Phi, Rho, herm(Phi)) - Rho));
+
+      // make Op the same size as our unit cell
+      Op = repeat(Op, UnitCellSize / Op.size());
+
+      std::map<std::complex<double>, Polynomial<MatrixOperator>, CompareComplex> 
+	 E = SolveMPO_Left(Phi, Psi.QShift, Op, Rho, Identity, Verbose);
+      Polynomial<std::complex<double> > aNorm = ExtractOverlap(E[1.0], Rho);
+
+      std::cout << "#degree #real #imag\n";
+      for (int i = 0; i <= aNorm.degree(); ++i)
+	 {
+	    std::cout << i << ' ' << aNorm[i].real() << ' ' << aNorm[i].imag() << '\n';
+	 }
+      std::cout << std::endl;
+
+      pheap::Shutdown();
+   }
+   catch (std::exception& e)
+   {
+      std::cerr << "Exception: " << e.what() << '\n';
       return 1;
    }
-
-   long CacheSize = getenv_or_default("MP_CACHESIZE", 655360);
-   pvalue_ptr<InfiniteWavefunction> PsiPtr = pheap::OpenPersistent(argv[1], CacheSize, true);
-   InfiniteWavefunction Psi = *PsiPtr;
-   int UnitCellSize = Psi.Psi.size();
-
-   TriangularOperator Op;
-
-   // Construct the operator
-
-   //   Op = ConstructIsingOperator();
-   //Op = ConstructHeisOperatorSU2();
-   //   Op = ConstructTriOperator();
-   Op = ConstructSpinOperator();
-   TRACE(Op);
-
-   // Make a LinearWavefunction in the symmetric orthogonality constraint
-   MatrixOperator Identity = MatrixOperator::make_identity(Psi.Psi.Basis1());
-   MatrixOperator Rho = scalar_prod(Psi.C_right, herm(Psi.C_right));
-   LinearWavefunction Phi = Psi.Psi; // no need to bugger around with C_old,C_right
- 
-   MatrixOperator LambdaSqrt = SqrtDiagonal(Psi.C_old);
-   MatrixOperator LambdaInvSqrt = InvertDiagonal(LambdaSqrt, InverseTol);
-
-   bool Verbose = true;
-
-   Phi.set_front(prod(LambdaInvSqrt, Phi.get_front()));
-   Phi.set_back(prod(Phi.get_back(), delta_shift(LambdaSqrt, adjoint(Psi.QShift))));
-   Rho = Psi.C_old;
-   Identity = Rho;
-
-   // Rho and Identity are the same matrix in this representation
-
-   //   TRACE(norm_frob(operator_prod(herm(Phi), Identity, Phi) - Identity));
-   //   TRACE(norm_frob(operator_prod(Phi, Rho, herm(Phi)) - Rho));
-
-   std::cout.precision(14);
-
-   // make Op the same size as our unit cell
-   Op = repeat(Op, UnitCellSize / Op.size());
-
-   std::map<std::complex<double>, Polynomial<MatrixOperator>, CompareComplex> 
-      E = SolveMPO_Left(Phi, Psi.QShift, Op, Rho, Identity, Verbose);
-   Polynomial<std::complex<double> > aNorm = ExtractOverlap(E[1.0], Rho);
-
-   std::cout << "#degree #real #imag\n";
-   for (int i = 0; i <= aNorm.degree(); ++i)
-      {
-         std::cout << i << ' ' << aNorm[i].real() << ' ' << aNorm[i].imag() << '\n';
-      }
-   std::cout << std::endl;
-
-   pheap::Shutdown();
+   catch (...)
+   {
+      std::cerr << "Unknown exception!\n";
+      return 1;
+   }
 }
