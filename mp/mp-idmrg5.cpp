f7f310040eb3006d01fd082c0e038c56a102ecdc
Ian McCulloch <ianmcc@physics.uq.edu.au>
2013-07-25 11:33:00 +0000

    Fix sign of omega in mp/mp-idmrg5.cpp

--- mp/mp-idmrg5.cpp
+++ mp/mp-idmrg5.cpp
@@ -1523,7 +1523,7 @@ int main(int argc, char** argv)
 	 Ham = -J * (TriangularTwoSite(Site["BH_S"], Site["B_S"]) + TriangularTwoSite(Site["B_S"], Site["BH_S"])
 		     + TriangularTwoSite(Site["BH_A"], Site["B_A"]) + TriangularTwoSite(Site["B_A"], Site["BH_A"]));
 
-	 SiteOperator LocalHamiltonian = Omega * (Site["N_S"] - Site["N_A"]);
+	 SiteOperator LocalHamiltonian = -Omega * (Site["N_S"] - Site["N_A"]);
 	 LocalHamiltonian += U * Site["N_S"] * Site["N_A"] 
 	    + 0.5 * (Site["N2_S"] + Site["N2_A"]) 
 	    + 0.25 * (Site["BH2_S"]*Site["B2_A"] + Site["BH2_A"]*Site["B2_S"]);
