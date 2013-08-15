84efdaf9b4267e91488041ed466defe5835fd8ba
Ian McCulloch <ianmcc@physics.uq.edu.au>
2013-08-15 01:05:47 +0000

    Added boost version information to copyright.h

--- mp/copyright.h
+++ mp/copyright.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <iomanip>
 #include "config.h"
+#include <boost/version.hpp>
 
 #define AS_STRING(X) AS_STRING2(X)
 #define AS_STRING2(X) #X
@@ -26,6 +27,9 @@ inline void print_copyright(std::ostream& out)
 {
    out << "Matrix Product Toolkit version " VERSION "\n"
       "Compiled on " __DATE__ " at " __TIME__ "\n"
+      "Using Boost version " << (BOOST_VERSION / 100000) 
+       << "." << (BOOST_VERSION / 100 % 1000)
+       << "." << (BOOST_VERSION % 100) << "\n"
       "Copyright (c) Ian McCulloch 1999-2010 All Rights Reserved\n"
       "For license conditions email " PACKAGE_BUGREPORT "\n"
       ;
