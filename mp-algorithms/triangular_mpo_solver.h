e30faf87450e82c530c4185a79c85c374170dda7
Ian McCulloch <ianmcc@physics.uq.edu.au>
2015-05-12 16:22:15 +0000

    Added comment to mp-algorithms/triangular_mpo_solver.h

--- mp-algorithms/triangular_mpo_solver.h
+++ mp-algorithms/triangular_mpo_solver.h
@@ -1,6 +1,17 @@
 // -*- C++ -*- $Id$
 //
 // Solver for triangular MPO's
+//
+// Assumptions:
+// We assume that the identity is in the scalar sector of the transfer matrix.
+// This should be true because we form the transfer matrix of a single wavefunction,
+// we don't allow <psi1|X|psi2>.
+// We assume that there is at most one eigenvalue 1 in each symmetry sector.  This allows
+// for wavefunctions that are non-injective due to symmetry (eg Z2 symmetry in the ordered phase).
+// We also allow for MPO's that have a non-trivial unitary on the diagonal, but again it should
+// have at most one eigenvalue 1 per symmetry sector.
+//
+
 
 #if !defined(MPTOOLKIT_MP_ALGORITHMS_TRIANGULAR_MPO_SOLVER_H)
 #define MPTOOLKIT_MP_ALGORITHMS_TRIANGULAR_MPO_SOLVER_H
