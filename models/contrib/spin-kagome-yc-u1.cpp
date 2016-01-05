6dc5bf9a4baa4cc91e5a730036076d56945ee03a
Seyed Saadatmand <s.saadatmand@uq.edu.au>
2016-01-05 13:18:37 +1000

    Adding two new model files to /contrib: spin-kagome-yc-u1.cpp & spin-kagome-yc-su2.cpp

---
+++ models/contrib/spin-kagome-yc-u1.cpp
@@ -0,0 +1,194 @@
+// -*- C++ -*- $Id: $
+// Descriptin: spin systems on kagome lattices with YC structure and efficient way of numbering; U(1)-symmetric. <spin-kagome-yc-u1.cpp>
+// Authors: Ian P. McCulloch and Seyed N. Saadatmand
+// Contact: s.saadatmand@uq.edu.au
+
+// YC configuration of a kagome lattice.
+// The default unit-cell size is '(3/2)*width' value, so the width should be even.
+//
+// Example for a 'width=6' lattice (site numbers in brackets are periodic repeats in 
+// the vertical direction; i.e. top-left (8) is the same site as the bottom-left 5).
+// Sites 9-to-17 are the second unit cell, e.g. 9 is (1)[0].
+//
+//                (26)
+//                 |
+//        (17)     18     
+//         |       | >19   
+//(8)      9       20       
+// |       | >10 < |    ...        
+// 0       11      21     
+// | > 1 < |       | >22   
+// 2       12      23  
+// |       | >13 < |    ...  
+// 3       14      24     
+// | > 4 < |       | >25    
+// 5       15      26  
+// |       | >16 < |        
+// 6       17     (18)   
+// | > 7 < |            
+// 8      (9)       
+// |             
+//(0)
+
+
+#include "pheap/pheap.h"
+#include "lattice/infinitelattice.h"
+#include "lattice/unitcelloperator.h"
+#include "mp/copyright.h"
+#include "models/spin-u1.h"
+#include "common/terminal.h"
+#include <boost/program_options.hpp>
+
+namespace prog_opt = boost::program_options;
+
+int main(int argc, char** argv)
+{
+   try
+   {
+      half_int Spin = 0.5;
+      int w = 4;
+      std::string FileName;
+
+      prog_opt::options_description desc("Allowed options", terminal::columns());
+      desc.add_options()
+         ("help", "show this help message")
+         ("Spin,S", prog_opt::value(&Spin), "magnitude of the spin [default 0.5]")
+	 ("width,w", prog_opt::value(&w), "width of the cylinder, should be even [default 4]")
+         ("out,o", prog_opt::value(&FileName), "output filename [required]")
+         ;
+      
+      prog_opt::variables_map vm;        
+      prog_opt::store(prog_opt::command_line_parser(argc, argv).
+                      options(desc).style(prog_opt::command_line_style::default_style ^
+					  prog_opt::command_line_style::allow_guessing).
+		      run(), vm);
+      prog_opt::notify(vm);    
+      
+      if (vm.count("help") || !vm.count("out"))
+      {
+         print_copyright(std::cerr);
+         std::cerr << "usage: " << basename(argv[0]) << " [options]\n";
+         std::cerr << desc << '\n';
+	 std::cerr << "Constructs a kagome lattice in the YC configuration with an\n"
+		   << "efficient way of numbering of 1D chain. The default unit-cell size\n" 
+                   << "is '(3/2)*width' value.\n"
+                   << "Operators:\n"
+		   << "H_J1    - nearest neighbor spin exchange\n"
+		   << "H_J2    - next-nearest neighbor spin exchange\n"
+                   << "Functions:\n"
+                   << "H( J1 = NN coupling strength, J2 = NNN coupling strength, theta = atan(J2/J1)\n"
+                   << "  \"radians\", alpha = J2/J1 )\n"   
+	    ;
+         return 1;
+      }
+
+      CHECK(w%2 == 0)(w)("Width must be an even integer!");
+
+      unsigned u = 3*(w/2);  // Hamiltonian unit-cell size
+
+      LatticeSite Site = SpinU1(Spin);
+      UnitCell Cell = repeat(Site, u);
+
+      // Add some operators on the unit cell
+      UnitCellOperator Sp(Cell, "Sp"), Sm(Cell, "Sm"), Sz(Cell, "Sz");
+      for (int i = 0; i < u; ++i)
+      {
+	 Sp += Sp[i];     // total spin on a leg of cylinder
+	 Sm += Sm[i];     // total spin on a leg of cylinder
+	 Sz += Sz[i];     // total spin on a leg of cylinder
+      }
+
+      // Now we construct the InfiniteLattice,
+      InfiniteLattice Lattice(Cell);
+
+      unsigned num_bonds_j1 = 0;
+      unsigned num_bonds_j2 = 0;
+
+      // Construct the Hamiltonian for a single unit-cell,
+      UnitCellMPO H1, H2;
+      for (int i = 0; i < u; ++i)
+      {
+	 // Nearest neighbor bonds
+
+	 // vertical bonds:
+	 if ( i%3 != 1 ) 
+         {
+           if ( i%3 == 0 )
+             H1 += Sz(0)[i]*Sz(0)[i+2] + 0.5 * (Sp(0)[i]*Sm(0)[i+2] + Sm(0)[i]*Sp(0)[i+2]);
+           else
+             H1 += Sz(0)[i]*Sz(0)[(i+1)%u] + 0.5 * (Sp(0)[i]*Sm(0)[(i+1)%u] + Sm(0)[i]*Sp(0)[(i+1)%u]);
+           ++num_bonds_j1;
+         }
+  
+	 // 60 degree bonds:
+	 if ( i%3 == 0 )
+           {
+             H1 += Sz(0)[i]*Sz(0)[i+1] + 0.5 * (Sp(0)[i]*Sm(0)[i+1] + Sm(0)[i]*Sp(0)[i+1]);
+             ++num_bonds_j1;
+           }
+         else if ( i%3 == 1 )
+           {
+             H1 += Sz(0)[i]*Sz(1)[i+1] + 0.5 * (Sp(0)[i]*Sm(1)[i+1] + Sm(0)[i]*Sp(1)[i+1]);   
+	     H1 += Sz(0)[i]*Sz(1)[(i+2)%u] + 0.5 * (Sp(0)[i]*Sm(1)[(i+2)%u] + Sm(0)[i]*Sp(1)[(i+2)%u]);
+             num_bonds_j1 += 2;
+           }
+         else if ( i%3 == 2 )
+           {
+             H1 += Sz(0)[i]*Sz(0)[i-1] + 0.5 * (Sp(0)[i]*Sm(0)[i-1] + Sm(0)[i]*Sp(0)[i-1]);
+             ++num_bonds_j1;
+           }
+ 
+	 // Next-nearest neighbor bonds
+	 
+	 // horizental bonds:
+	 if ( i%3 != 1 )
+         {
+           if ( i%3 == 0 )
+             H2 += Sz(0)[i]*Sz(1)[i+2] + 0.5 * (Sp(0)[i]*Sm(1)[i+2] + Sm(0)[i]*Sp(1)[i+2]);
+           else   
+             H2 += Sz(0)[i]*Sz(1)[(i+1)%u] + 0.5 * (Sp(0)[i]*Sm(1)[(i+1)%u] + Sm(0)[i]*Sp(1)[(i+1)%u]);
+           ++num_bonds_j2;
+         }
+
+         // inclined bonds:
+         if ( i%3 == 0 )
+           {
+             H2 += Sz(0)[i]*Sz(0)[(i-2+u)%u] + 0.5 * (Sp(0)[i]*Sm(0)[(i-2+u)%u] + Sm(0)[i]*Sp(0)[(i-2+u)%u]);
+             ++num_bonds_j2;
+           }
+         else if ( i%3 == 1 )
+           {
+             H2 += Sz(0)[i]*Sz(1)[i-1] + 0.5 * (Sp(0)[i]*Sm(1)[i-1] + Sm(0)[i]*Sp(1)[i-1]);
+             H2 += Sz(0)[i]*Sz(1)[(i+4)%u] + 0.5 * (Sp(0)[i]*Sm(1)[(i+4)%u] + Sm(0)[i]*Sp(1)[(i+4)%u]);
+             num_bonds_j2 += 2;
+           }
+         else if ( i%3 == 2 )
+           {
+             H2 += Sz(0)[i]*Sz(0)[(i+2)%u] + 0.5 * (Sp(0)[i]*Sm(0)[(i+2)%u] + Sm(0)[i]*Sp(0)[(i+2)%u]);
+             ++num_bonds_j2;
+           }
+      }
+
+      std::cout << "UnitCell size is:" << " " << u << std::endl;  
+      std::cout << "The number of J1 bonds per unit-cell:" << " " << num_bonds_j1 << ", and the number of J2 bonds per unit-cell: " << num_bonds_j2 << std::endl;
+
+      Lattice["H_J1"] = sum_unit(H1);
+      Lattice["H_J2"] = sum_unit(H2);
+
+      Lattice.func("H")(arg("J1") = "cos(theta)", arg("J2") = "sin(theta)", arg("theta") = "atan(alpha)", arg("alpha") = 0.0)
+	 = "J1*H_J1 + J2*H_J2";
+
+      // save the lattice
+      pheap::ExportObject(FileName, Lattice);
+   }
+   catch (std::exception& e)
+   {
+      std::cerr << "Exception: " << e.what() << '\n';
+      return 1;
+   }
+   catch (...)
+   {
+      std::cerr << "Unknown exception!\n";
+      return 1;
+   }
+}
---
+++ models/contrib/spin-tri-yc-SU2.cpp
@@ -0,0 +1,248 @@
+// -*- C++ -*- $Id: spin-tri-yc-SU2.cpp 1490 2015-05-19 09:15:06Z seyed $
+// Authors: Ian P. McCulloch and Seyed N. Saadatmand
+// Contact: s.saadatmand@uq.edu.au
+
+// <obelix> @ /data5/uqssaada/git/mptoolkit/models/contrib/
+
+// YC configuration of a triangular lattice.
+// The default unit-cell size is the width value.
+//
+// Example for a width-6 lattice (site numbers in brackets are periodic repeats in the vertical
+// direction (i.e. top-left (5) is the same site as the bottom-left 5).
+// Sites 6,7,8,9,10,11 are the second unit cell, e.g. 6 is (1)[0].
+//
+//      (17)
+//  (11) |
+//(5) | 12
+// |  6< |
+// 0< | 13
+// |  7< |
+// 1< | 14
+// |  8< | ...
+// 2< | 15
+// |  9< |
+// 3< | 16
+// | 10< |
+// 4< | 17
+// | 11< |
+// 5< | (12)
+// | (6)
+//(0)
+
+
+#include "pheap/pheap.h"
+#include "lattice/infinitelattice.h"
+#include "lattice/unitcelloperator.h"
+#include "mp/copyright.h"
+#include "models/spin-su2.h"
+#include "common/terminal.h"
+#include <boost/program_options.hpp>
+
+
+namespace prog_opt = boost::program_options;
+
+
+int IntPow(int x, int p) {
+  if (p == 0) return 1;
+  if (p == 1) return x;
+  return x * IntPow(x, p-1);
+}
+
+// Functions that produces dimer terms for the Hamiltonian (including their h.c. or rotated term): 
+
+UnitCellMPO DimerKinetic(half_int spin, int width, int cell1, int site1, int cell2, int site2, int cell3, int site3, int cell4, int site4) {
+
+ LatticeSite Site = SpinSU2(spin);
+ UnitCell Cell = repeat(Site, width);
+ UnitCellOperator S(Cell, "S"), I(Cell, "I");
+ UnitCellMPO H, Dots, DoubleDots, CrossDots;
+
+ Dots = inner(S(cell1)[site1], S(cell4)[site4]) + inner(S(cell2)[site2], S(cell3)[site3]) - inner(S(cell1)[site1], S(cell2)[site2]) -   
+        inner(S(cell3)[site3], S(cell4)[site4]) - inner(S(cell1)[site1], S(cell3)[site3]) - inner(S(cell2)[site2], S(cell4)[site4]);
+
+ DoubleDots = inner(S(cell1)[site1], S(cell2)[site2])*inner(S(cell3)[site3], S(cell4)[site4]) +  
+              inner(S(cell1)[site1], S(cell3)[site3])*inner(S(cell2)[site2], S(cell4)[site4]);
+
+ CrossDots = inner(cross(S(cell1)[site1], S(cell2)[site2]), cross(S(cell3)[site3], S(cell4)[site4])) +
+             inner(cross(S(cell1)[site1], S(cell3)[site3]), cross(S(cell2)[site2], S(cell4)[site4]));
+
+ H = 0.125*I + 0.25*Dots + 0.5*DoubleDots - 0.125*CrossDots;
+
+ return H;
+
+}
+
+UnitCellMPO DimerPotential(half_int spin, int width, int cell1, int site1, int cell2, int site2, int cell3, int site3, int cell4, int site4) {
+
+ LatticeSite Site = SpinSU2(spin);
+ UnitCell Cell = repeat(Site, width);
+ UnitCellOperator S(Cell, "S"), I(Cell, "I");
+ UnitCellMPO H, P12, P34, P13, P24;
+
+ P12 = 0.25*I - inner(S(cell1)[site1], S(cell2)[site2]);
+ P34 = 0.25*I - inner(S(cell3)[site3], S(cell4)[site4]);
+
+ P13 = 0.25*I - inner(S(cell1)[site1], S(cell3)[site3]);
+ P24 = 0.25*I - inner(S(cell2)[site2], S(cell4)[site4]);
+
+ H = P12*P34 + P13*P24;
+
+ return H;
+
+}
+
+
+int main(int argc, char** argv)
+{
+   try
+   {
+      half_int Spin = 0.5;
+      int w = 3;
+      std::string FileName;
+
+      prog_opt::options_description desc("Allowed options", terminal::columns());
+      desc.add_options()
+         ("help", "show this help message")
+         ("Spin,S", prog_opt::value(&Spin), "magnitude of the spin [default 0.5]")
+	 ("width,w", prog_opt::value(&w), "width of the cylinder [default 3]")
+         ("out,o", prog_opt::value(&FileName), "output filename [required]")
+         ;
+      
+      prog_opt::variables_map vm;        
+      prog_opt::store(prog_opt::command_line_parser(argc, argv).
+                      options(desc).style(prog_opt::command_line_style::default_style ^
+					  prog_opt::command_line_style::allow_guessing).
+		      run(), vm);
+      prog_opt::notify(vm);    
+      
+      if (vm.count("help") || !vm.count("out"))
+      {
+         print_copyright(std::cerr);
+         std::cerr << "usage: " << basename(argv[0]) << " [options]\n";
+         std::cerr << desc << '\n';
+	 std::cerr << "Constructs a triangular lattice in the YC configuration with wrapping vector (0,1),\n"
+		   << "while spins sit on edges. This employs an efficient way of numbering in the 1D chain.\n\n"
+                   << "Operators:\n"
+		   << "H_J1    - nearest neighbor spin exchange\n"
+		   << "H_J2    - next-nearest neighbor spin exchange\n"
+                   << "H_t     - kinetic term of quantum dimer model's Hamiltonian on the triangular lattice\n"
+                   << "H_v     - potential term of quantum dimer model's Hamiltonian on the triangular lattice\n"
+                   << "S       - total spin on a leg of the cylinder\n"
+                   << "StagS   - staggered magnetization over a unit-cell\n\n"
+                   << "Trans   - translation by one site (rotation by 2\u0071/w) in lattice short direction\n"
+                   << "Ref     - reflection in lattice short direction (may need applying T-operators to become gneral reflection)\n"
+		   << "If the lattice could be potentially tripartite (width is a multiple of 3), then we\n"
+		   << "define sublattice spin operators on a \"width*3\" unit cells as,\n"
+		   << "S_A     - tripartite sublattice spin, including site S(0)[0]\n"
+		   << "S_B     - tripartite sublattice spin, including site S(0)[1]\n"
+		   << "S_C     - tripartite sublattice spin, including site S(0)[2]\n\n"
+                   << "Functions:\n"
+                   << "H( J1 = NN coupling strength, J2 = NNN coupling strength, theta = atan(J2/J1)\n" 
+                   << "  \"radians\", alpha = J2/J1 )\n\n"                         
+	    ;
+         return 1;
+      }
+
+      LatticeSite Site = SpinSU2(Spin);
+      UnitCell Cell = repeat(Site, w);
+      UnitCellOperator S(Cell, "S"), StagS(Cell, "StagS");
+      UnitCellOperator I(Cell, "I"); // identity operator
+      UnitCellOperator Trans(Cell, "Trans"), Ref(Cell, "Ref");
+
+      // Add some operators on the unit-cell
+   
+      for (int i = 0; i < w; ++i)
+      {
+	 S += S[i];                                         // total spin on a leg of cylinder
+         StagS += IntPow(-1,i) * S[i];            
+      }
+
+      Trans = I(0);
+      for (int i = 0; i < w-1; ++i)
+       {
+           //T *= 0.5*( 0.25*inner(S[i],S[i+1]) + 1 );
+           Trans = Trans(0) * Cell.swap_gate_no_sign(i, i+1);
+       }
+
+      Ref = I(0);
+      for (int i = 0; i < w/2; ++i)
+       {
+           //R *= 0.5*( 0.25*inner(S[i],S[w-i-1]) + 1 );    // old way of representing R-operator.
+           Ref = Ref(0) * Cell.swap_gate_no_sign(i, w-i-1);
+       }
+          
+      // if we could have tripartite symmetry, add operators for the sublattice magnetization
+      UnitCellMPO S_A, S_B, S_C;
+
+      if (w%3 == 0)
+      {
+	 for (int i = 0; i < w; i += 3)
+	 {
+	    S_A += S(0)[i]   + S(1)[(i+2)%w] + S(2)[(i+1)%w];
+	    S_B += S(0)[i+1] + S(1)[(i+3)%w] + S(2)[(i+2)%w];
+	    S_C += S(0)[i+2] + S(1)[(i+4)%w] + S(2)[(i+3)%w];
+	 }
+      }
+
+
+      // Now we construct the InfiniteLattice,
+      InfiniteLattice Lattice(Cell);
+
+      // Construct the Hamiltonian for a single unit-cell,
+      UnitCellMPO H1, H2, Ht, Hv;
+    
+      for (int i = 0; i < w; ++i)
+      {
+	// nearest neighbor bonds
+	 // vertical bonds
+	 H1 += inner(S(0)[i], S(0)[(i+1)%w]);
+	 // 60 degree bonds
+	 H1 += inner(S(0)[i], S(1)[i]);
+	 H1 += inner(S(0)[i], S(1)[(i+1)%w]);
+
+	// next-nearest neighbor bonds
+	 H2 += inner(S(0)[i], S(2)[(i+1)%w]);                  // horizontal
+	 H2 += inner(S(0)[i], S(1)[(i+w-1)%w]);                // up-right
+	 H2 += inner(S(0)[i], S(1)[(i+2)%w]);                  // down-right
+         
+        // kinetic terms of the dimer model
+         Ht += DimerKinetic(Spin, w, 0, i, 1, i, 1, (i+1)%w, 2, (i+1)%w);               // horizontal rhombus terms
+         Ht += DimerKinetic(Spin, w, 0, i, 0, (i+1)%w, 1, i, 1, (i+1)%w);               // upper-vertical rhombus terms
+         Ht += DimerKinetic(Spin, w, 0, i, 1, (i+1)%w, 0, (i+1)%w, 1, (i+2)%w);         // lower-vertical rhombus terms
+
+        // potential terms of the dimer model
+         Hv += DimerPotential(Spin, w, 0, i, 1, i, 1, (i+1)%w, 2, (i+1)%w);             // horizontal rhombus terms
+         Hv += DimerPotential(Spin, w, 0, i, 0, (i+1)%w, 1, i, 1, (i+1)%w);             // upper-vertical rhombus terms
+         Hv += DimerPotential(Spin, w, 0, i, 1, (i+1)%w, 0, (i+1)%w, 1, (i+2)%w);       // lower-vertical rhombus terms
+      }
+
+      Lattice["H_J1"] = sum_unit(H1);
+      Lattice["H_J2"] = sum_unit(H2);
+      Lattice["H_t"] = sum_unit(Ht);
+      Lattice["H_v"] = sum_unit(Hv);
+
+      Lattice.func("H")(arg("J1") = "cos(theta)", arg("J2") = "sin(theta)", arg("theta") = "atan(alpha)", arg("alpha") = 0.0)
+	 = "J1*H_J1 + J2*H_J2";
+
+      // Add the tripartite sublattice magnetization operators
+      if (w%3 == 0)
+      {
+	 Lattice["S_A"] = sum_unit(S_A, w*3);
+	 Lattice["S_B"] = sum_unit(S_B, w*3);
+	 Lattice["S_C"] = sum_unit(S_C, w*3);
+      }
+
+      // save the lattice
+      pheap::ExportObject(FileName, Lattice);
+   }
+   catch (std::exception& e)
+   {
+      std::cerr << "Exception: " << e.what() << '\n';
+      return 1;
+   }
+   catch (...)
+   {
+      std::cerr << "Unknown exception!\n";
+      return 1;
+   }
+}
