8a2a96631aebc5e12fdef00d3ae9d812893e16df
Ian McCulloch <ianmcc@physics.uq.edu.au>
2016-01-27 00:37:45 +1000

    Put the DY error back into utils/linearfit

--- utils/linearfit.cpp
+++ utils/linearfit.cpp
@@ -13,14 +13,14 @@ int main()
    double x, y, dy;
    vector<double> X, Y, DY;
    cout.precision(20);
-   while (cin >> x >> y)
+   while (cin >> x >> y >> dy)
    {
       X.push_back(x);
       Y.push_back(y);
-      DY.push_back(y);  // DY is the variance, not the standard deviation
+      DY.push_back(dy);  // DY is the variance, not the standard deviation
    }
 
    linear_fit_result R = linear_fit(X.begin(), X.end(), Y.begin(), DY.begin());
 
-   cout << R.c << '\n'; // ' ' << sqrt(R.variance_c) << '\n';
+   cout << R.c << '\n' << ' ' << sqrt(R.variance_c) << '\n';
 }
