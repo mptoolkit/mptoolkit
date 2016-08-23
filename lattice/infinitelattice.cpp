mptoolkit/mptoolkit

commit 702c4a7d71113fbf01cf83ec806a933a44d5d877

Added conditional operator support for OperatorDescriptions, and pass UnitCell by pointer to InfiniteLattice
This is an optional 3rd or 4th parameter to the add_operators()(Name, Description, [Conditional], [Function]) function
The third parameter is a string, if this is not empty then the operator is treated as a conditional operator
and the condition is listed in the help string for the model.  Additionally, a 4th parameter is a function,
anything that can be represented in a std::function<bool()>, which is invoked to test whether the function should be
defined and print a warning if not (and vice versa).  This 4th parameter is optional, if it is absent then no warning
will be emitted if the operator is not defined.

The 4th parameter, if it is specified, will typically be a lambda function that takes some parameter by reference (since
we need the value at the point where the lambda is invoked, not when it is constructed), and return true if the operator
should be defined.  For example,

      OpDescriptions.add_operators()
	 ("StagS"      , "staggered magnetization", "width even", [&w]()->bool {return w%2 == 0;})
         ;

where w is a parameter to the model that is set on the command line.

The UnitCell can now optionally be passed to the InfiniteLattice by pointer.  If this is done, then it is assumed
that the UnitCell has a longer lifetime than the InfiniteLattice.  This is generally true if they are both constructed
in the same scope.  This now allows modifications to the unit cell after the Lattice has been constructed.
This requires updating all models, since there is no longer a conversion ctor that takes
a non-rvalue UnitCell.  The options are:

1. pass the unit cell by pointer.

      LatticeSite Site = SpinSU2(Spin);
      UnitCell Cell = repeat(Site, w);
      InfiniteLattice Lattice(&Cell);

2. pass the unit cell from a temporary

      LatticeSite Site = SpinSU2(Spin);
      InfiniteLattice Lattice(repeat(Site, w));

lattice/infinitelattice.cpp
@@ -23,20 +23,95 @@
 
 PStream::VersionTag LatticeVersion(3);
 
-InfiniteLattice::InfiniteLattice()
+InfiniteLattice::InfiniteLattice() : UnitCell_(new UnitCell()), OwnUnitCell_(true)
 {
 }
 
-InfiniteLattice::InfiniteLattice(UnitCell const& uc)
-   : UnitCell_(uc)
+InfiniteLattice::InfiniteLattice(UnitCell* uc)
+   : UnitCell_(uc), OwnUnitCell_(false)
 {
 }
 
-InfiniteLattice::InfiniteLattice(std::string const& Description, UnitCell const& uc)
-   : Description_(Description), UnitCell_(uc)
+InfiniteLattice::InfiniteLattice(UnitCell&& uc)
+   : UnitCell_(new UnitCell(std::move(uc))), OwnUnitCell_(true)
 {
 }
 
+InfiniteLattice::InfiniteLattice(std::string const& Description, UnitCell& uc)
+   : Description_(Description), UnitCell_(&uc), OwnUnitCell_(false)
+{
+}
+
+
+InfiniteLattice::InfiniteLattice(std::string const& Description, UnitCell&& uc)
+   : Description_(Description), UnitCell_(new UnitCell(std::move(uc))), OwnUnitCell_(true)
+{
+}
+
+InfiniteLattice::InfiniteLattice(InfiniteLattice const& Other)
+   : Description_(Other.Description_),
+     Authors_(Other.Authors_),
+     CommandLine_(Other.CommandLine_),
+     Timestamp_(Other.Timestamp_),
+     UnitCell_(Other.OwnUnitCell_ ? Other.UnitCell_ : new UnitCell(*Other.UnitCell_)),
+     OwnUnitCell_(Other.OwnUnitCell_),
+     Operators_(Other.Operators_),
+     Arguments_(Other.Arguments_),
+     Functions_(Other.Functions_)
+{
+}
+ 
+InfiniteLattice::InfiniteLattice(InfiniteLattice&& Other)
+   : Description_(std::move(Other.Description_)),
+     Authors_(std::move(Other.Authors_)),
+     CommandLine_(std::move(Other.CommandLine_)),
+     Timestamp_(std::move(Other.Timestamp_)),
+     UnitCell_(std::move(UnitCell_)),
+     OwnUnitCell_(std::move(Other.OwnUnitCell_)),
+     Operators_(std::move(Other.Operators_)),
+     Arguments_(std::move(Other.Arguments_)),
+     Functions_(std::move(Other.Functions_))
+{
+   Other.UnitCell_ = nullptr;
+   Other.OwnUnitCell_ = false;
+}
+
+InfiniteLattice&
+InfiniteLattice::operator=(InfiniteLattice const& Other)
+{
+   Description_ = Other.Description_;
+   Authors_ = Other.Authors_;
+   CommandLine_ = Other.CommandLine_;
+   Timestamp_ = Other.Timestamp_;
+   UnitCell_ = Other.OwnUnitCell_ ? Other.UnitCell_ : new UnitCell(*Other.UnitCell_);
+   OwnUnitCell_ = Other.OwnUnitCell_;
+   Operators_ = Other.Operators_;
+   Arguments_ = Other.Arguments_;
+   Functions_ = Other.Functions_;
+}
+
+InfiniteLattice&
+InfiniteLattice::operator=(InfiniteLattice&& Other)
+{
+   Description_ = std::move(Other.Description_);
+   Authors_ = std::move(Other.Authors_);
+   CommandLine_ = std::move(Other.CommandLine_);
+   Timestamp_ = std::move(Other.Timestamp_);
+   UnitCell_ = std::move(Other.UnitCell_);
+   OwnUnitCell_ = std::move(Other.OwnUnitCell_);
+   Operators_ = std::move(Other.Operators_);
+   Arguments_ = std::move(Other.Arguments_);
+   Functions_ = std::move(Other.Functions_);
+   Other.UnitCell_ = nullptr;
+   Other.OwnUnitCell_ = false;
+}
+
+InfiniteLattice::~InfiniteLattice()
+{
+   if (OwnUnitCell_)
+      delete UnitCell_;
+}
+   
 void
 InfiniteLattice::set_command_line(int argc, char** argv)
 {
@@ -69,18 +144,42 @@ InfiniteLattice::set_operator_descriptions(OperatorDescriptions const& Desc)
    // iterate through the descriptions
    for (OperatorDescriptions::const_iterator I = Desc.begin(); I != Desc.end(); ++I)
    {
-      if (this->operator_exists(I->first))
+      if (this->operator_exists(std::get<0>(*I)))
       {
-	 Operators_[I->first].set_description(I->second);
+	 // see if the operator is conditional
+	 if (std::get<3>(*I) && (!(*std::get<3>(*I))()))
+	 {
+	    std::cerr << "warning: conditional lattice operator " << std::get<0>(*I) 
+		      << " (conditional on: " << std::get<2>(*I) << ") should not be defined, but is!\n";
+	 }
+	 Operators_[std::get<0>(*I)].set_description(std::get<1>(*I));
       }
       else
       {
-	 std::cerr << "warning: operator " << I->first 
-		   << " has a description but is not defined in the lattice.\n";
+	 // is the operator optional?
+	 if (!std::get<2>(*I).empty() || std::get<3>(*I))
+	 {
+	    // yes, check and see that we satisfy the condition
+	    if (std::get<3>(*I))
+	    {
+	       // invoke the function
+	       if (((*std::get<3>(*I))()))
+	       {
+		  std::cerr << "warning: conditional lattice operator "  << std::get<0>(*I) 
+			    << " should be defined but is not.\n";
+	       }
+	    }
+	 }
+	 else
+	 {
+	    std::cerr << "warning: operator " << std::get<0>(*I)
+		      << " has a description but is not defined in the lattice.\n";
+	 }
       }
    }
 
-   // Now go through the operators and check for any that don't have a description
+   // Now go through the operators and check for any that don't have a description, or are optional but
+   // should not have been defined
    for (const_operator_iterator I = this->begin_operator(); I != this->end_operator(); ++I)
    {
       if (I->second.description().empty())
@@ -122,14 +221,37 @@ InfiniteLattice::set_operator_descriptions(OperatorDescriptions const& Desc)
    // cell operator descriptions
    for (OperatorDescriptions::const_iterator I = Desc.cell_begin(); I != Desc.cell_end(); ++I)
    {
-      if (this->GetUnitCell().operator_exists(I->first))
+      if (this->GetUnitCell().operator_exists(std::get<0>(*I)))
       {
-	 this->GetUnitCell()[I->first].set_description(I->second);
+	 // see if the operator is conditional
+	 if (std::get<3>(*I) && (!(*std::get<3>(*I))()))
+	 {
+	    std::cerr << "warning: conditional unit cell operator " << std::get<0>(*I) 
+		      << " (conditional on: " << std::get<2>(*I) << ") should not be defined, but is!\n";
+	 }
+	 this->GetUnitCell()[std::get<0>(*I)].set_description(std::get<1>(*I));
       }
       else
       {
-	 std::cerr << "warning: cell operator " << I->first
-		   << " has a description but is not defined in the unit cell.\n";
+	 // is the operator optional?
+	 if (!std::get<2>(*I).empty() || std::get<3>(*I))
+	 {
+	    // yes, check and see that we satisfy the condition
+	    if (std::get<3>(*I))
+	    {
+	       // invoke the function
+	       if (((*std::get<3>(*I))()))
+	       {
+		  std::cerr << "warning: conditional unit cell operator "  << std::get<0>(*I) 
+			    << "should be defined but is not.\n";
+	       }
+	    }
+	 }
+	 else
+	 {
+	    std::cerr << "warning: cell operator " << std::get<0>(*I)
+		      << " has a description but is not defined in the unit cell.\n";
+	 }
       }
    }
 
@@ -137,7 +259,7 @@ InfiniteLattice::set_operator_descriptions(OperatorDescriptions const& Desc)
    for (UnitCell::const_operator_iterator I = this->GetUnitCell().begin_operator();
 	I != this->GetUnitCell().end_operator(); ++I)
    {
-      if (I->second.description().empty())
+      if (std::get<1>(*I).description().empty())
       {
 	 std::cerr << "warning: cell operator " << I->first << " has no description.\n";
       }
@@ -255,7 +377,7 @@ operator<<(PStream::opstream& out, InfiniteLattice const& L)
    out << L.Authors_;
    out << L.CommandLine_;
    out << L.Timestamp_;
-   out << L.UnitCell_;
+   out << *L.UnitCell_;
    out << L.Operators_;
    out << L.Arguments_;
    out << L.Functions_;
@@ -288,7 +410,13 @@ operator>>(PStream::ipstream& in, InfiniteLattice& L)
    }
    in >> L.CommandLine_;
    in >> L.Timestamp_;
-   in >> L.UnitCell_;
+   if (L.OwnUnitCell_)
+   {
+      delete L.UnitCell_;
+   }
+   L.OwnUnitCell_ = true;
+   L.UnitCell_ = new UnitCell();
+   in >> (*L.UnitCell_);
    in >> L.Operators_;
    in >> L.Arguments_;
    in >> L.Functions_;

lattice/infinitelattice.h
@@ -24,7 +24,8 @@
 // infinite product of operators defined on the unit cell, and also have a QShift, with
 // the requirement that Basis1() == delta_shift(Basis2(), QShift).
 //
-// The unit cell of the operators is allowed to a a multiple of the lattice UnitCell.
+// The unit cell of the operators is allowed to a multiple of the lattice UnitCell.
+// We do some fancy tracking to keep the UnitCell by pointer where possible
 
 #if !defined(MPTOOLKIT_LATTICE_INFINITELATTICE_H)
 #define MPTOOLKIT_LATTICE_INFINITELATTICE_H
@@ -65,12 +66,23 @@ class InfiniteLattice
 
       InfiniteLattice();
 
-      explicit InfiniteLattice(UnitCell const& uc);
+      explicit InfiniteLattice(UnitCell* uc);
+      explicit InfiniteLattice(UnitCell&& uc);
+   //      explicit InfiniteLattice(UnitCell const& uc);  // maybe confusing to have this?
 
-      InfiniteLattice(std::string const& Description, UnitCell const& uc);
+      InfiniteLattice(std::string const& Description, UnitCell& uc);
+      InfiniteLattice(std::string const& Description, UnitCell&& uc);
 
-      UnitCell& GetUnitCell() { return UnitCell_; }
-      UnitCell const& GetUnitCell() const { return UnitCell_; }
+      InfiniteLattice(InfiniteLattice const& Other);
+      InfiniteLattice(InfiniteLattice&& Other);
+
+      InfiniteLattice& operator=(InfiniteLattice const& Other);
+      InfiniteLattice& operator=(InfiniteLattice&& Other);
+
+      ~InfiniteLattice();
+
+      UnitCell& GetUnitCell() { return *UnitCell_; }
+      UnitCell const& GetUnitCell() const { return *UnitCell_; }
 
       std::string description() const { return Description_; }
       void set_description(std::string s) { Description_ = s; }
@@ -166,7 +178,8 @@ class InfiniteLattice
       authors_type Authors_;
       std::string CommandLine_;
       std::string Timestamp_;
-      UnitCell UnitCell_;
+      UnitCell* UnitCell_;
+      bool OwnUnitCell_;               // true if UnitCell_ is owned by us on the heap
       OperatorListType Operators_;
       ArgumentListType Arguments_;
       FunctionListType Functions_;

lattice/latticesite.cpp
@@ -97,23 +97,46 @@ LatticeSite::set_operator_descriptions(OperatorDescriptions const& Desc)
    // iterate through the descriptions
    for (OperatorDescriptions::const_iterator I = Desc.begin(); I != Desc.end(); ++I)
    {
-      if (this->operator_exists(I->first))
+      if (this->operator_exists(std::get<0>(*I)))
       {
-	 this->operator[](I->first).set_description(I->second);
+	 // check if it was a conditional operator that should not be defined
+	 if (std::get<3>(*I) && (!(*std::get<3>(*I))()))
+	 {
+	    std::cerr << "warning: conditional local operator " << std::get<0>(*I) 
+		      << " (conditional on: " << std::get<2>(*I) << ") should not be defined, but is!\n";
+	 }
+	 this->operator[](std::get<0>(*I)).set_description(std::get<1>(*I));
       }
       else
       {
-	 std::cerr << "warning: operator " << I->first 
-		   << " has a description but is not defined in the lattice site.\n";
+	 // is the operator optional?
+	 if (!std::get<2>(*I).empty() || std::get<3>(*I))
+	 {
+	    // yes, check and see that we satisfy the condition
+	    if (std::get<3>(*I))
+	    {
+	       // invoke the function
+	       if (((*std::get<3>(*I))()))
+	       {
+		  std::cerr << "warning: conditional local operator "  << std::get<0>(*I) 
+			    << "should be defined but is not.\n";
+	       }
+	    }
+	 }
+	 else
+	 {
+	    std::cerr << "warning: operator " << std::get<0>(*I)
+		      << " has a description but is not defined in the lattice site.\n";
+	 }
       }
    }
 
    // Now go through the operators and check for any that don't have a description
    for (const_operator_iterator I = this->begin_operator(); I != this->end_operator(); ++I)
    {
-      if (I->second.description().empty())
+      if (std::get<1>(*I).description().empty())
       {
-	 std::cerr << "warning: local operator " << I->first << " has no description.\n";
+	 std::cerr << "warning: local operator " << std::get<0>(*I) << " has no description.\n";
       }
    }
 }

lattice/operator_descriptions.h
@@ -36,11 +36,15 @@
 #include <map>
 #include <iostream>
 #include <iomanip>
+#include <functional>
+#include <boost/optional.hpp>
 
 class OperatorDescriptions
 {
    public:
-      typedef std::pair<std::string, std::string> value_type;
+      // tuple of: name, description, conditional description, conductional function (optional)
+      typedef boost::optional<std::function<bool()>> ftype;
+      typedef std::tuple<std::string, std::string, std::string, boost::optional<std::function<bool()>>> value_type;
       typedef std::vector<value_type> data_type;
 
       typedef data_type::const_iterator const_iterator;
@@ -82,14 +86,48 @@ class OperatorDescriptions
             if (Index.find(Name) == Index.end())
             {
                Index[Name] = Descriptions.size();
-               Descriptions.push_back(std::make_pair(Name, Desc));
+               Descriptions.push_back(std::make_tuple(Name, Desc, "", ftype()));
             }
             else
             {
-               Descriptions[Index[Name]] = std::make_pair(Name, Desc);
+               Descriptions[Index[Name]] = std::make_tuple(Name, Desc, "", ftype());
             }
             return *this;
          }
+
+         OperatorDescProxy const& operator()(std::string const& Name,
+                                             std::string const& Desc,
+					     std::string const& Condition) const
+	 {
+            if (Index.find(Name) == Index.end())
+            {
+               Index[Name] = Descriptions.size();
+               Descriptions.push_back(std::make_tuple(Name, Desc, Condition, ftype()));
+            }
+            else
+            {
+               Descriptions[Index[Name]] = std::make_tuple(Name, Desc, Condition, ftype());
+            }
+            return *this;
+         }
+
+         OperatorDescProxy const& operator()(std::string const& Name,
+                                             std::string const& Desc,
+					     std::string const& Condition,
+					     std::function<bool()> Test) const
+	 {
+            if (Index.find(Name) == Index.end())
+            {
+               Index[Name] = Descriptions.size();
+               Descriptions.push_back(std::make_tuple(Name, Desc, Condition, Test));
+            }
+            else
+            {
+               Descriptions[Index[Name]] = std::make_tuple(Name, Desc, Condition, Test);
+            }
+            return *this;
+         }
+
          data_type& Descriptions;
          index_type& Index;
       };
@@ -158,9 +196,28 @@ std::ostream& operator<<(std::ostream& out, OperatorDescriptions const& d)
    out << "Operators:\n";
    if (d.size() == 0)
       out << "(none)\n";
+   // divide up into various conditions
+   std::set<std::string> Conditions;
    for (OperatorDescriptions::const_iterator I = d.begin(); I != d.end(); ++I)
    {
-      out << std::setw(10) << std::left << I->first << " - " << I->second << '\n';
+      if (!std::get<2>(*I).empty() || std::get<3>(*I))
+      {
+	 Conditions.insert(std::get<2>(*I));
+      }
+      else
+	 out << std::setw(10) << std::left << std::get<0>(*I) << " - " << std::get<1>(*I) << '\n';
+   }
+   // iterate over the possible conditions
+   for (std::string const& m : Conditions)
+   {
+      out << "\nOperators conditional on: " << m << "\n";
+      for (OperatorDescriptions::const_iterator I = d.begin(); I != d.end(); ++I)
+      {
+	 if (std::get<2>(*I) == m && (!m.empty() || std::get<3>(*I)))
+	 {
+	    out << std::setw(10) << std::left << std::get<0>(*I) << " - " << std::get<1>(*I) << '\n';
+	 }
+      }
    }
 
    out << "\nFunctions:\n";

commit d698f027dfa9b5010bf7165ee67d57308d2669a1

Fix bugs introduced in 702c4a7d71113fbf01cf83ec806a933a44d5d877 in the InfiniteLattice constructors

lattice/infinitelattice.cpp
@@ -53,7 +53,7 @@ InfiniteLattice::InfiniteLattice(InfiniteLattice const& Other)
      Authors_(Other.Authors_),
      CommandLine_(Other.CommandLine_),
      Timestamp_(Other.Timestamp_),
-     UnitCell_(Other.OwnUnitCell_ ? Other.UnitCell_ : new UnitCell(*Other.UnitCell_)),
+     UnitCell_(Other.OwnUnitCell_ ? new UnitCell(*Other.UnitCell_) : Other.UnitCell_),
      OwnUnitCell_(Other.OwnUnitCell_),
      Operators_(Other.Operators_),
      Arguments_(Other.Arguments_),
@@ -66,7 +66,7 @@ InfiniteLattice::InfiniteLattice(InfiniteLattice&& Other)
      Authors_(std::move(Other.Authors_)),
      CommandLine_(std::move(Other.CommandLine_)),
      Timestamp_(std::move(Other.Timestamp_)),
-     UnitCell_(std::move(UnitCell_)),
+     UnitCell_(std::move(Other.UnitCell_)),
      OwnUnitCell_(std::move(Other.OwnUnitCell_)),
      Operators_(std::move(Other.Operators_)),
      Arguments_(std::move(Other.Arguments_)),
@@ -83,7 +83,7 @@ InfiniteLattice::operator=(InfiniteLattice const& Other)
    Authors_ = Other.Authors_;
    CommandLine_ = Other.CommandLine_;
    Timestamp_ = Other.Timestamp_;
-   UnitCell_ = Other.OwnUnitCell_ ? Other.UnitCell_ : new UnitCell(*Other.UnitCell_);
+   UnitCell_ = Other.OwnUnitCell_ ? new UnitCell(*Other.UnitCell_) : Other.UnitCell_;
    OwnUnitCell_ = Other.OwnUnitCell_;
    Operators_ = Other.Operators_;
    Arguments_ = Other.Arguments_;