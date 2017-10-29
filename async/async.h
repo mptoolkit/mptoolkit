036a7d7a2b60547c7472f6811fa6335ed4f26305
Ian McCulloch <ianmcc@physics.uq.edu.au>
2017-10-30 08:47:05 +1000

    cuda musings

--- async/async.h
+++ async/async.h
@@ -20,6 +20,26 @@
 #if !defined(MPTOOLKIT_ASYNC_ASYNC_H)
 #define MPTOOLKIT_ASYNC_ASYNC_H
 
+// thread pool design:
+//
+// we try to keep one thread spinning.  (maybe if there are no queues
+// we could sleep.)  The thread goes through the list of queues
+// and tries to find a queue that has a runnable task.  To do that,
+// we first grab a spinlock from the queue (eg using an atomic_flag,
+// see http://en.cppreference.com/w/cpp/atomic/atomic_flag).  We then
+// see if the queue is runnable.  If not, then drop the spinlock and try the 
+// next queue.
+// If we can run a task then signal the thread pool, which will wake up another
+// thread.  Suggest that we only ever have one thread reading the queue list
+// at a time.  In fact it can be protected by a mutex.  If the thread finds
+// a queue that ia defunct then it can remove it immediately.  So threads in the
+// thread pool will be blocked on that mutex, unless they are actually running
+// or can get the mutex and search the queue list.
+//
+// Adding tasks to a queue will typically be done by a different thread.  So
+// this needs to be threadsafe.  It can be a simple std::list, queue or boost
+// lock-free queue.
+
 namespace async
 {
 
@@ -60,6 +80,47 @@ class task_simple : public task
 // the user must ensure that there is enough synchronization to ensure that
 // the event is recorded before the task executes, the task object itself
 // may be created arbitarily far before the event is initialized.
+//
+// Note that we cannot schedule an event before it is recorded because that
+// would violate normal ordering.  That is, we need to do a thread synchronization
+// surrounding blocking calls.  Typical scenario:
+//
+// 1. A = B*C
+// 2. D = eigenvalues(A)
+// 3. E = B*A
+// If we run asynchronously then step 3 cannot be scheduled until after
+// step 2 has completed, and we cannot use cuda events to synchronize this.
+// When we schedule step 3, we need to keep in mind that the event is not yet
+// recorded.  The async gpu matrix needs to store the event by reference.
+// Is it sufficient to do reference-counted events?  Probably not needed -
+// we don't need to reference count memory buffers etc because the destructor
+// is itself an async event so normal scheduling will ensure that the destructor
+// doesn't run early (this means that the task must take ownership via move
+// semantics when destroying).  Is all of this solved by simply storing a
+// pointer to a gpu matrix?  No, we must need some amount of additional syncronization,
+// consider the case where some queue (thread) is running and waiting for some
+// other queue.  eg
+//
+// Queue 1:
+// A = B;
+// A *= 2;
+//
+// Queue 2:
+// C = B*A;
+//
+// Queue 2 must be ordered with respect to the operations in queue 1,
+// in order to get the right semantics and get the correct cuda event pointer.
+// That is, C waits for A, so A must have already recorded the event.  That is,
+// we cannot schedule the cuda kernel for C until after we have scheduled the
+// appropriate operations in queue 1.  That is, the cuda calls need to
+// be serialized appropriately.
+// How efficient does the queue synchronization need to be?  We can use a similar
+// mechanism, but it should be fairly efficient.  Is polling good enough?
+//
+// If we want to have the thread hold a mutex on the task list while running, 
+// we could have a separate list for newly added tasks.
+
+
 class task_wait_gpu : public task
 {
    public:
