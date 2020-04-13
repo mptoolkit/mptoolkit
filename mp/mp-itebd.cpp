72d893a05110b2ab26671eeca4dd4405923c5c53
Ian McCulloch <ianmcc@physics.uq.edu.au>
2020-04-14 03:23:46 +1000

    Improved mp-itebd to support multiple Lie-Trotter-Suzuki decompositions, following the recipes from https://arxiv.org/abs/1901.04974

--- mp/mp-itebd.cpp
+++ mp/mp-itebd.cpp
@@ -64,6 +64,112 @@ std::string FormatDigits(double x, int Digits)
    return str.str();
 }
 
+// generic Lie-Trotter-Suzuki decomposition with two slices, A and B.
+class LTSDecomposition
+{
+   public:
+      LTSDecomposition() : Order_(0) {}
+      LTSDecomposition(int Order, std::string Description,
+                       std::vector<double> a,
+                       std::vector<double> b)
+         : Order_(Order), Description_(Description), a_(a), b_(b)
+      {
+         CHECK(a.size() == b.size() || a.size() == b.size()+1);
+         CHECK_CLOSE(std::accumulate(a.begin(), a.end(), 0.0), 1.0);
+         CHECK_CLOSE(std::accumulate(b.begin(), b.end(), 0.0), 1.0);
+      }
+
+      int Order_;
+      std::string Description_;
+      std::vector<double> a_;
+      std::vector<double> b_;
+};
+
+// Symmetric decompositions
+// Even slice A, odd slice B.
+// We always have either a.size() == b.size() OR a.size() == b.size()+1.
+// If a.size() == b.size()+1 = n+1, then the pattern is A_1 B_1 A_2 B_2 ... A_N B_N A_{N+1} B_N A_N .... B_2 A_2 B_1 A_1
+// if b.size() == a.size() = n, then the pattern is A_1 B_1 A_2 B_2 ... A_N B_N A_{N-1} B_{N-2} A_{N-2} .... B_1 A_1
+// We don't need to include the final term of each array on construction since it is implicit:
+// If a.size() == b.size()+1 then add final term to a of 1.0 - 2*sum(a) and final term to b of 0.5 - sum(b)
+// If a.size() == b.size() then add final term to a of 0.5 - sum(a), and final term to b of 1.0 - 2*sum(b)
+
+LTSDecomposition
+SymmetricDecomposition(int Order, std::string Description,
+                       std::vector<double> a,
+                       std::vector<double> b)
+{
+   if (a.size() == b.size())
+   {
+      a.push_back(0.5 - std::accumulate(a.begin(), a.end(), 0.0));
+      b.push_back(1.0 - 2.0 * std::accumulate(b.begin(), b.end(), 0.0));
+
+      int asz = a.size();
+      for (int i = asz-1; i >=0; --i)
+         a.push_back(a[i]);
+
+      int bsz = b.size();
+      for (int i = bsz-2; i >= 0; --i)
+         b.push_back(b[i]);
+   }
+   else if (a.size() == b.size()+1)
+   {
+      a.push_back(1.0 - 2.0 * std::accumulate(a.begin(), a.end(), 0.0));
+      b.push_back(0.5 - std::accumulate(b.begin(), b.end(), 0.0));
+
+      int asz = a.size();
+      for (int i = asz-2; i >=0; --i)
+         a.push_back(a[i]);
+
+      int bsz = b.size();
+      for (int i = bsz-1; i >= 0; --i)
+         b.push_back(b[i]);
+   }
+   else
+   {
+      PANIC("Invalid SymmetricDecomposition");
+   }
+
+   return LTSDecomposition(Order, Description, a, b);
+}
+
+// The LeapfrogDecomposition assumes that the number of terms (m-1)/2 is odd, otherwise we need to repeat
+// the final term again. We only need to supply n-1 weights, since the final weight is always
+// 1 - 2 * sum(w)
+// This is a product of leapfrog terms
+// U(w) = e^{0.5*w*A} e^{w*B} r^{0.5*w*A}
+// and for $n$ terms the expansion is U(w_1) U(w_2) ... U(w_{n-1}) U(w_n) U(w_{n-1}) ... U(w_1) ((m-1)/2 odd)
+// or U(w_1) U(w_2) ... U(w_{n-1}) U(w_n) U(w_n) U(w_{n-1}) ... U(w_1) ((m-1)/2 even)
+// We only need to supply (n-1) weights, as the final weight is 1 - 2*sum_{i<n}(w_i)
+LTSDecomposition
+LeapfrogDecompositionOdd(int Order, std::string Description, std::vector<double> w)
+{
+   std::vector<double> a;
+   std::vector<double> b;
+   //w.push_back(1.0 - 2.0 * std::accumulate(w.begin(), w.end(), 0.0));
+   double aa = 0;
+   for (auto ww : w)
+   {
+      a.push_back(0.5*(aa+ww));
+      b.push_back(ww);
+      aa = ww;
+   }
+   return SymmetricDecomposition(Order, Description, a, b);
+}
+
+std::map<std::string, LTSDecomposition>
+Decompositions = {
+   {"leapfrog2",      LeapfrogDecompositionOdd(2, "Traditional 2nd order 3-term leapfrog decomposition", {})},
+   {"optimized2-5",   SymmetricDecomposition(2, "Optimized 2nd order 5-term decomposition",
+                                             {0.211324865405187}, {})},
+   {"optimized4-11",  SymmetricDecomposition(4, "Optimized 4th order 11-term decomposition",
+                                             {0.095848502741203681182, -0.078111158921637922695},
+                                             {0.42652466131587616168, -0.12039526945509726545})},
+   {"symmetric6-19",  LeapfrogDecompositionOdd(6, "6th order leapfrog 19-term decomposition",
+                                               {0.18793069262651671457, 0.5553,
+                                                     0.12837035888423653774, -0.84315275357471264676})}};
+
+
 #if 0
 void DoTEBD(StateComponent& A, StateComponent& B, RealDiagonalOperator& Lambda,
             SimpleOperator const& U, StatesInfo const& Info)
@@ -166,37 +272,42 @@ DoTEBD(StateComponent& A, StateComponent& B, RealDiagonalOperator& Lambda,
 }
 #endif
 
-LinearWavefunction
-DoEvenStep(std::deque<StateComponent> Psi, std::deque<RealDiagonalOperator> Lambda,
-           SimpleOperator const& UEven, StatesInfo const& SInfo)
-{
-   unsigned Sz = Psi.size();
-   std::vector<TruncationInfo> TInfo(Sz/2);
-   #pragma omp parallel for schedule(dynamic)
-   for (unsigned i = 0; i < Sz; i += 2)
-   {
-      TInfo[i/2] = DoTEBD(Psi[i], Psi[i+1], Lambda[i/2], UEven, SInfo);
-   }
-   for (unsigned i = 0; i < Sz; i += 2)
-   {
-      std::cout << "Bond=" << (i+1)
-                << " Entropy=" << TInfo[i/2].TotalEntropy()
-                << " States=" << TInfo[i/2].KeptStates()
-                << " Trunc=" << TInfo[i/2].TruncationError()
-                << '\n';
-   }
-   std::cout << std::flush;
-   return LinearWavefunction::FromContainer(Psi.begin(), Psi.end());
-}
-
-void DoEvenOddStep(std::deque<StateComponent>& Psi, std::deque<RealDiagonalOperator>& Lambda,
-                   QuantumNumber const& QShift,
-                   SimpleOperator const& UEven, SimpleOperator const& UOdd, StatesInfo const& SInfo)
+void DoEvenSlice(std::deque<StateComponent>& Psi,
+                 std::deque<RealDiagonalOperator>& Lambda,
+                 SimpleOperator const& UEven,
+                 StatesInfo const& SInfo,
+                 int Verbose)
 {
    // physical state is A B (Lambda) C D (Lambda) ...
    // All A,B,C,D,... are left canonical
    // After even slice, the state is
    // A (Lambda) B C (Lambda) D E (Lambda) ...
+   unsigned Sz = Psi.size();
+   int MaxStates = 0;
+   for (unsigned i = 0; i < Sz; i += 2)
+   {
+      TruncationInfo Info = DoTEBD(Psi[i], Psi[i+1], Lambda[i/2], UEven, SInfo);
+      if (Verbose > 0)
+      {
+         std::cout << "Bond=" << (i+1)
+                   << " Entropy=" << Info.TotalEntropy()
+                   << " States=" << Info.KeptStates()
+                   << " Trunc=" << Info.TruncationError()
+                   << '\n';
+      }
+      MaxStates = std::max(MaxStates, Info.KeptStates());
+   }
+   std::cout << "Even slice finished, max states=" << MaxStates << '\n';
+   std::cout << std::flush;
+}
+
+void DoOddSlice(std::deque<StateComponent>& Psi,
+                std::deque<RealDiagonalOperator>& Lambda,
+                QuantumNumber const& QShift,
+                SimpleOperator const& UOdd,
+                StatesInfo const& SInfo,
+                int Verbose)
+{
    // In preparation for the odd slice, we rotate to
    // Z A (Lambda) B C (Lambda) D E (Lambda) ...
    // After the odd slice, we have
@@ -204,38 +315,23 @@ void DoEvenOddStep(std::deque<StateComponent>& Psi, std::deque<RealDiagonalOpera
    // which we need to rotate back to
    // A B (Lambda) C D (Lambda) ..... Z (Lambda)
    unsigned Sz = Psi.size();
-   std::vector<TruncationInfo> TInfo(Sz/2);
-   #pragma omp parallel for schedule(dynamic)
-   for (unsigned i = 0; i < Sz; i += 2)
-   {
-      TInfo[i/2] = DoTEBD(Psi[i], Psi[i+1], Lambda[i/2], UEven, SInfo);
-   }
-   for (unsigned i = 0; i < Sz; i += 2)
-   {
-      std::cout << "Bond=" << (i+1)
-                << " Entropy=" << TInfo[i/2].TotalEntropy()
-                << " States=" << TInfo[i/2].KeptStates()
-                << " Trunc=" << TInfo[i/2].TruncationError()
-                << '\n';
-   }
-   std::cout << std::flush;
-
-   // need to the pair that wraps around separately
    Psi.push_front(delta_shift(Psi.back(), QShift));
    Psi.pop_back();
-   #pragma omp parallel for schedule(dynamic)
+  int MaxStates = 0;
    for (unsigned i = 0; i < Sz; i += 2)
    {
-      TInfo[i/2] = DoTEBD(Psi[i], Psi[i+1], Lambda[i/2], UOdd, SInfo);
+      TruncationInfo Info = DoTEBD(Psi[i], Psi[i+1], Lambda[i/2], UOdd, SInfo);
+      if (Verbose > 0)
+      {
+         std::cout << "Bond=" << i
+                   << " Entropy=" << Info.TotalEntropy()
+                   << " States=" << Info.KeptStates()
+                   << " Trunc=" << Info.TruncationError()
+                   << '\n';
+      }
+      MaxStates = std::max(MaxStates, Info.KeptStates());
    }
-   for (unsigned i = 0; i < Sz; i += 2)
-   {
-      std::cout << "Bond=" << i
-                << " Entropy=" << TInfo[i/2].TotalEntropy()
-                << " States=" << TInfo[i/2].KeptStates()
-                << " Trunc=" << TInfo[i/2].TruncationError()
-                << '\n';
-   }
+   std::cout << "Odd slice finished, max states=" << MaxStates << '\n';
    std::cout << std::flush;
 
    Psi.push_back(delta_shift(Psi.front(), adjoint(QShift)));
@@ -259,11 +355,12 @@ int main(int argc, char** argv)
       std::string Operator;
       std::string Operator2;
       int MinStates = 1;
-      int States = 100;
+      int States = 100000;
       double TruncCutoff = 0;
       double EigenCutoff = 1E-16;
       int OutputDigits = 0;
       int Coarsegrain = 1;
+      std::string DecompositionStr = "leapfrog2";
 
       prog_opt::options_description desc("Allowed options", terminal::columns());
       desc.add_options()
@@ -273,6 +370,7 @@ int main(int argc, char** argv)
 	 ("operator", prog_opt::value(&Operator), "operator for the first slice ST decomposition")
 	 ("operator2", prog_opt::value(&Operator2), "operator for the second slice ST decomposition [optional]")
 	 ("timestep,t", prog_opt::value(&TimestepStr), "timestep (required)")
+         ("decomposition,c", prog_opt::value(&DecompositionStr), FormatDefault("choice of decomposition", DecompositionStr).c_str())
 	 ("num-timesteps,n", prog_opt::value(&N), FormatDefault("number of timesteps to calculate", N).c_str())
 	 ("save-timesteps,s", prog_opt::value(&SaveEvery), "save the wavefunction every s timesteps")
          ("min-states", prog_opt::value<int>(&MinStates),
@@ -300,11 +398,28 @@ int main(int argc, char** argv)
          print_copyright(std::cerr, "tools", "mp-itebd");
          std::cerr << "usage: " << basename(argv[0]) << " [options]\n";
          std::cerr << desc << '\n';
+         std::cerr << "\nAvailable decompositions:\n";
+         for (auto const& d : Decompositions)
+         {
+            std::cerr << d.first << " : " << d.second.Description_ << '\n';
+         }
          return 1;
       }
 
       std::cout.precision(getenv_or_default("MP_PRECISION", 14));
       std::cerr.precision(getenv_or_default("MP_PRECISION", 14));
+
+      LTSDecomposition decomp;
+      for (auto const& d : Decompositions)
+      {
+         if (d.first == DecompositionStr)
+            decomp = d.second;
+      }
+      if (decomp.Order_ == 0)
+      {
+         std::cerr << "mp-itebd: fatal: invalid decomposition\n";
+         return 1;
+      }
 
       if (Verbose > 0)
          std::cout << "Loading wavefunction..." << std::endl;
@@ -365,10 +480,33 @@ int main(int argc, char** argv)
       SimpleOperator EvenX = coarse_grain(EvenOp.MPO()).scalar();
       SimpleOperator OddX = coarse_grain(EvenOp.MPO()).scalar();
 
-      SimpleOperator EvenU = Exponentiate(std::complex<double>(0, -Timestep) * EvenX);
-      SimpleOperator OddU = Exponentiate(std::complex<double>(0, -Timestep) * OddX);
+      std::vector<SimpleOperator> EvenU;
+      for (auto x : decomp.a_)
+      {
+         EvenU.push_back(Exponentiate(std::complex<double>(0, -Timestep * x) * EvenX));
+      }
 
-      SimpleOperator EvenUHalf = Exponentiate(std::complex<double>(0, -Timestep/2) * EvenX);
+      std::vector<SimpleOperator> OddU;
+      for (auto x : decomp.b_)
+      {
+         OddU.push_back(Exponentiate(std::complex<double>(0, -Timestep * x) * OddX));
+      }
+
+      // If we have an odd number of terms (the usual case), then we can wrap around the last even slice
+      // if we are continuing the evolution beyond the current timestep.  This is the sum of a[last] + a[0] terms
+      SimpleOperator EvenContinuation;
+      if (decomp.a_.size() == decomp.b_.size()+1)
+      {
+         double x = decomp.a_.front() + decomp.a_.back();
+         EvenContinuation = Exponentiate(std::complex<double>(0, -Timestep * x) * EvenX);
+      }
+
+      std::cout << "Using decomposition " << DecompositionStr << '\n';
+      if (Verbose > 0)
+      {
+         std::cout << "Number of even slices: " << EvenU.size() << '\n';
+         std::cout << "Number of odd slices: " << OddU.size() << '\n';
+      }
 
       StatesInfo SInfo;
       SInfo.MinStates = MinStates;
@@ -378,17 +516,13 @@ int main(int argc, char** argv)
 
       std::cout << SInfo << '\n';
 
-      if (Psi.size() == 1)
-         Psi = repeat(Psi, 2);
-
       if (Psi.size()%2 != 0)
       {
-         std::cerr << "mp-itebd: fatal: wavefunction must be multiple of 2 sites.\n";
-         return 1;
+         std::cerr << "mp-itebd: warning: wavefunction is not a multiple of 2 sites, doubling unit cell...\n";
+         Psi = repeat(Psi, 2);
       }
 
       QuantumNumber QShift = Psi.qshift();
-
 
       std::deque<StateComponent> PsiVec(Psi.begin(), Psi.end());
       std::deque<RealDiagonalOperator> Lambda;
@@ -401,42 +535,69 @@ int main(int argc, char** argv)
       if (SaveEvery == 0)
          SaveEvery = N;
 
-      // the initial half timestep
-      int tstep = 1;
-      DoEvenOddStep(PsiVec, Lambda, QShift, EvenUHalf, OddU, SInfo);
-      std::cout << "Timestep " << tstep << " time " << (InitialTime+tstep*Timestep) << '\n';
+      // If Continue then we merge the final (even) slice of one timestep with the first slice of
+      // the next timestep
+      bool Continue = false;
+      int tstep = 0;
 
       while (tstep < N)
       {
-         while (tstep % SaveEvery != 0)
+         if (Continue)
          {
-            DoEvenOddStep(PsiVec, Lambda, QShift, EvenU, OddU, SInfo);
-            ++tstep;
-            std::cout << "Timestep " << tstep << " time " << (InitialTime+tstep*Timestep) << '\n';
+            if (Verbose > 1)
+            {
+               std::cout << "Merge slice\n";
+            }
+            DoEvenSlice(PsiVec, Lambda, EvenContinuation, SInfo, Verbose);
+         }
+         else
+         {
+            DoEvenSlice(PsiVec, Lambda, EvenU[0], SInfo, Verbose);
          }
 
-         // evolve the final half step and save the wavefunction
-         LinearWavefunction Psi = DoEvenStep(PsiVec, Lambda, EvenUHalf, SInfo);
-
-         // save the wavefunction
-         std::cout << "Saving wavefunction\n";
-         MPWavefunction Wavefunction;
-         std::string TimeStr = FormatDigits(InitialTime + tstep * Timestep, OutputDigits);
-         InfiniteWavefunctionLeft PsiL = InfiniteWavefunctionLeft::Construct(Psi, QShift);
-         Wavefunction.Wavefunction() = std::move(PsiL);
-         Wavefunction.AppendHistoryCommand(EscapeCommandline(argc, argv));
-         Wavefunction.SetDefaultAttributes();
-         Wavefunction.Attributes()["Time"] = TimeStr;
-         Wavefunction.Attributes()["Prefix"] = OutputPrefix;
-         *PsiPtr.mutate() = std::move(Wavefunction);
-
-         pheap::ExportHeap(OutputPrefix + TimeStr, PsiPtr);
-
-         if (tstep+1 < N)
+         DoOddSlice(PsiVec, Lambda, QShift, OddU[0], SInfo, Verbose);
+         for (int bi = 1; bi < OddU.size(); ++bi)
          {
-            DoEvenOddStep(PsiVec, Lambda, QShift, EvenU, OddU, SInfo);
-            ++tstep;
-            std::cout << "Timestep " << tstep << " time " << (InitialTime+tstep*Timestep) << '\n';
+            DoEvenSlice(PsiVec, Lambda, EvenU[bi], SInfo, Verbose);
+            DoOddSlice(PsiVec, Lambda, QShift, OddU[bi], SInfo, Verbose);
+         }
+
+         // do we do a continuation?
+         Continue = EvenU.size() > OddU.size();
+
+         ++tstep;
+         std::cout << "Timestep " << tstep << " time " << (InitialTime+tstep*Timestep) << '\n';
+
+         // do we save the wavefunction?
+         if ((tstep % SaveEvery) == 0 || tstep == N)
+         {
+            LinearWavefunction Psi;
+            if (EvenU.size() > OddU.size())
+            {
+               CHECK_EQUAL(EvenU.size(), OddU.size()+1);
+               std::cout << "Doing final slice before saving wavefunction.\n";
+               // do the final slice to finish the timstep.  Make a copy of the wavefunction since it is better to
+               // avoid a truncation step and 'continue' the wavefunction by wrapping around the next timestep.
+               std::deque<StateComponent> PsiVecSave = PsiVec;
+               std::deque<RealDiagonalOperator> LambdaSave = Lambda;
+               DoEvenSlice(PsiVecSave, LambdaSave, EvenU.back(), SInfo, Verbose);
+               Psi = LinearWavefunction::FromContainer(PsiVecSave.begin(), PsiVecSave.end());
+            }
+            else
+               Psi = LinearWavefunction::FromContainer(PsiVec.begin(), PsiVec.end());
+
+            // save the wavefunction
+            std::cout << "Saving wavefunction\n";
+            MPWavefunction Wavefunction;
+            std::string TimeStr = FormatDigits(InitialTime + tstep * Timestep, OutputDigits);
+            InfiniteWavefunctionLeft PsiL = InfiniteWavefunctionLeft::Construct(Psi, QShift);
+            Wavefunction.Wavefunction() = std::move(PsiL);
+            Wavefunction.AppendHistoryCommand(EscapeCommandline(argc, argv));
+            Wavefunction.SetDefaultAttributes();
+            Wavefunction.Attributes()["Time"] = TimeStr;
+            Wavefunction.Attributes()["Prefix"] = OutputPrefix;
+            *PsiPtr.mutate() = std::move(Wavefunction);
+            pheap::ExportHeap(OutputPrefix + TimeStr, PsiPtr);
          }
       }
    }
