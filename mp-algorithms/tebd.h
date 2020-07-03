ae75d4f229a5ed82dbdd15562f511dbfa77ef99f
Ian McCulloch <ianmcc@physics.uq.edu.au>
2020-07-03 12:28:39 +1000

    Updated mp-itebd to use the decomposition of the Hamiltonian.  Old mp-itebd is renamed to mp-itebd-old

---
+++ mp-algorithms/tebd.h
@@ -0,0 +1,61 @@
+// -*- C++ -*-
+//----------------------------------------------------------------------------
+// Matrix Product Toolkit http://physics.uq.edu.au/people/ianmcc/mptoolkit/
+//
+// mp-algorithms/tebd.h
+//
+// Copyright (C) 2020 Ian McCulloch <ianmcc@physics.uq.edu.au>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Reseach publications making use of this software should include
+// appropriate citations and acknowledgements as described in
+// the file CITATIONS in the main source directory.
+//----------------------------------------------------------------------------
+// ENDHEADER
+
+#if !defined(MTOOLKIT_MP_ALGORITHMS_TEBD_H)
+#define MTOOLKIT_MP_ALGORITHMS_TEBD_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include "mps/state_component.h"
+#include "mps/truncation.h"
+
+// returns the number of digits of precision used in the decimal number f
+int Digits(std::string const& f);
+
+std::string FormatDigits(double x, int Digits);
+
+// generic Lie-Trotter-Suzuki decomposition with two slices, A and B.
+class LTSDecomposition
+{
+   public:
+      LTSDecomposition() : Order_(0) {}
+      LTSDecomposition(int Order, std::string Description,
+                       std::vector<double> a,
+                       std::vector<double> b)
+         : Order_(Order), Description_(Description), a_(a), b_(b)
+      {
+         CHECK(a.size() == b.size() || a.size() == b.size()+1);
+         CHECK_CLOSE(std::accumulate(a.begin(), a.end(), 0.0), 1.0);
+         CHECK_CLOSE(std::accumulate(b.begin(), b.end(), 0.0), 1.0);
+      }
+
+      int Order_;
+      std::string Description_;
+      std::vector<double> a_;
+      std::vector<double> b_;
+};
+
+extern std::map<std::string, LTSDecomposition> Decompositions;
+
+TruncationInfo
+DoTEBD(StateComponent& A, StateComponent& B, RealDiagonalOperator& Lambda,
+       SimpleOperator const& U, StatesInfo const& SInfo);
+
+#endif
--- mp/mp-itebd-old.cpp
+++ mp/mp-itebd-old.cpp
@@ -17,7 +17,6 @@
 //----------------------------------------------------------------------------
 // ENDHEADER
 
-#include "mp-algorithms/tebd.h"
 #include "mpo/basic_triangular_mpo.h"
 #include "wavefunction/infinitewavefunctionleft.h"
 #include "wavefunction/mpwavefunction.h"
@@ -31,15 +30,252 @@
 #include "common/environment.h"
 #include "interface/inittemp.h"
 #include "common/prog_options.h"
-#include "lattice/infinite-parser.h"
+#include "lattice/unitcell_mpo.h"
+#include "lattice/unitcell-parser.h"
 #include <cctype>
 #include "interface/inittemp.h"
 
 namespace prog_opt = boost::program_options;
 
+// returns the number of digits of precision used in the decimal number f
+int Digits(std::string const& f)
+{
+   std::string::const_iterator dot = std::find(f.begin(), f.end(), '.');
+   if (dot == f.end())
+      return 0;
+   ++dot;
+   std::string::const_iterator x = dot;
+   while (x != f.end() && isdigit(*x))
+      ++x;
+   if (x != f.end() && (*x == 'e' || *x == 'E'))
+   {
+      // exponential notation
+      ++x;
+      int Exp = std::stoi(std::string(x, f.end()));
+      return std::max(0, int((x-dot) - Exp));
+   }
+   return x-dot;
+}
+
+std::string FormatDigits(double x, int Digits)
+{
+   std::ostringstream str;
+   str << std::fixed << std::setprecision(Digits) << std::setfill('0') << x;
+   return str.str();
+}
+
+// generic Lie-Trotter-Suzuki decomposition with two slices, A and B.
+class LTSDecomposition
+{
+   public:
+      LTSDecomposition() : Order_(0) {}
+      LTSDecomposition(int Order, std::string Description,
+                       std::vector<double> a,
+                       std::vector<double> b)
+         : Order_(Order), Description_(Description), a_(a), b_(b)
+      {
+         CHECK(a.size() == b.size() || a.size() == b.size()+1);
+         CHECK_CLOSE(std::accumulate(a.begin(), a.end(), 0.0), 1.0);
+         CHECK_CLOSE(std::accumulate(b.begin(), b.end(), 0.0), 1.0);
+      }
+
+      int Order_;
+      std::string Description_;
+      std::vector<double> a_;
+      std::vector<double> b_;
+};
+
+// Symmetric decompositions
+// Even slice A, odd slice B.
+// We always have either a.size() == b.size() OR a.size() == b.size()+1.
+// If a.size() == b.size()+1 = n+1, then the pattern is A_1 B_1 A_2 B_2 ... A_N B_N A_{N+1} B_N A_N .... B_2 A_2 B_1 A_1
+// if b.size() == a.size() = n, then the pattern is A_1 B_1 A_2 B_2 ... A_N B_N A_{N-1} B_{N-2} A_{N-2} .... B_1 A_1
+// We don't need to include the final term of each array on construction since it is implicit:
+// If a.size() == b.size()+1 then add final term to a of 1.0 - 2*sum(a) and final term to b of 0.5 - sum(b)
+// If a.size() == b.size() then add final term to a of 0.5 - sum(a), and final term to b of 1.0 - 2*sum(b)
+
+LTSDecomposition
+SymmetricDecomposition(int Order, std::string Description,
+                       std::vector<double> a,
+                       std::vector<double> b)
+{
+   if (a.size() == b.size())
+   {
+      a.push_back(0.5 - std::accumulate(a.begin(), a.end(), 0.0));
+      b.push_back(1.0 - 2.0 * std::accumulate(b.begin(), b.end(), 0.0));
+
+      int asz = a.size();
+      for (int i = asz-1; i >=0; --i)
+         a.push_back(a[i]);
+
+      int bsz = b.size();
+      for (int i = bsz-2; i >= 0; --i)
+         b.push_back(b[i]);
+   }
+   else if (a.size() == b.size()+1)
+   {
+      a.push_back(1.0 - 2.0 * std::accumulate(a.begin(), a.end(), 0.0));
+      b.push_back(0.5 - std::accumulate(b.begin(), b.end(), 0.0));
+
+      int asz = a.size();
+      for (int i = asz-2; i >=0; --i)
+         a.push_back(a[i]);
+
+      int bsz = b.size();
+      for (int i = bsz-1; i >= 0; --i)
+         b.push_back(b[i]);
+   }
+   else
+   {
+      PANIC("Invalid SymmetricDecomposition");
+   }
+
+   return LTSDecomposition(Order, Description, a, b);
+}
+
+// The LeapfrogDecomposition assumes that the number of terms (m-1)/2 is odd, otherwise we need to repeat
+// the final term again. We only need to supply n-1 weights, since the final weight is always
+// 1 - 2 * sum(w)
+// This is a product of leapfrog terms
+// U(w) = e^{0.5*w*A} e^{w*B} r^{0.5*w*A}
+// and for $n$ terms the expansion is U(w_1) U(w_2) ... U(w_{n-1}) U(w_n) U(w_{n-1}) ... U(w_1) ((m-1)/2 odd)
+// or U(w_1) U(w_2) ... U(w_{n-1}) U(w_n) U(w_n) U(w_{n-1}) ... U(w_1) ((m-1)/2 even)
+// We only need to supply (n-1) weights, as the final weight is 1 - 2*sum_{i<n}(w_i)
+LTSDecomposition
+LeapfrogDecompositionOdd(int Order, std::string Description, std::vector<double> w)
+{
+   std::vector<double> a;
+   std::vector<double> b;
+   //w.push_back(1.0 - 2.0 * std::accumulate(w.begin(), w.end(), 0.0));
+   double aa = 0;
+   for (auto ww : w)
+   {
+      a.push_back(0.5*(aa+ww));
+      b.push_back(ww);
+      aa = ww;
+   }
+   return SymmetricDecomposition(Order, Description, a, b);
+}
+
+std::map<std::string, LTSDecomposition>
+Decompositions = {
+   {"firstorder",     LTSDecomposition(1, "First order decomposition", {1.0}, {1.0})},
+   {"leapfrog2",      LeapfrogDecompositionOdd(2, "Traditional 2nd order 3-term leapfrog decomposition", {})},
+   {"optimized2-5",   SymmetricDecomposition(2, "Optimized 2nd order 5-term decomposition",
+                                             {0.211324865405187}, {})},
+   {"optimized4-11",  SymmetricDecomposition(4, "Optimized 4th order 11-term decomposition",
+                                             {0.095848502741203681182, -0.078111158921637922695},
+                                             {0.42652466131587616168, -0.12039526945509726545})},
+   {"symmetric6-19",  LeapfrogDecompositionOdd(6, "6th order leapfrog 19-term decomposition",
+                                               {0.18793069262651671457, 0.5553,
+                                                     0.12837035888423653774, -0.84315275357471264676})}};
+
+
+#if 0
+void DoTEBD(StateComponent& A, StateComponent& B, RealDiagonalOperator& Lambda,
+            SimpleOperator const& U, StatesInfo const& Info)
+{
+   // Algorithm avoids matrix inversion.  Let A,B be left-orthogonalized.
+   // Let C^{s1,s2} = A^{s1} B^{s2}
+   // Let C'^{s1,s2} = U(C^{s1,s2}) is the unitary gate applied to C
+
+   // Let X^{s1,s2} = C'^{s1,s2} lambda_2
+
+   // Do an SVD of X.  X^{s1,s2} = A'{s1} lambda_1 B'{s2}
+   // Use orthogonality of A': sum_{s1} A'\dagger{s1} A'{s1} = I
+   // sum_{s1} A'\dagger{s1} C'{s1,s2} = lambda_1 B'{s2} lambda_2^{-1} = G^{s2}
+
+   // Then form D^{s2,s1} = G^{s2} A^{s1}
+   // and so on
+
+   StateComponent C = local_tensor_prod(A,B);
+   StateComponent Cu = local_prod(U, C);
+
+   StateComponent X = Cu * Lambda;
+   AMatSVD SL(X, Tensor::ProductBasis<BasisList, BasisList>(A.LocalBasis(), B.LocalBasis()));
+   TruncationInfo Info;
+   AMatSVD::const_iterator Cutoff = TruncateFixTruncationError(SL.begin(), SL.end(),
+                                                               SInfo, Info);
+   std::cout << " Entropy=" << Info.TotalEntropy()
+             << " States=" << Info.KeptStates()
+             << " Trunc=" << Info.TruncationError()
+             << std::endl;
+
+   SL.ConstructMatrices(SL.begin(), Cutoff, A, Lambda, B);
+
+   StateComponent G = partial_prod(herm(A), Cu,
+                                   Tensor::ProductBasis<BasisList, BasisList>(A.LocalBasis(),
+                                                                              B.LocalBasis()));
+
+   B = A;
+   A = Lambda*G;
+}
+#else
+LinearAlgebra::DiagonalMatrix<double>
+InvertDiagonal(LinearAlgebra::DiagonalMatrix<double> const& D, double Tol = 1E-15)
+{
+   LinearAlgebra::DiagonalMatrix<double> Result(size1(D), size2(D));
+   for (unsigned i = 0; i < size1(D); ++i)
+   {
+      Result.diagonal()[i] = norm_frob(D.diagonal()[i]) < Tol ? 0.0 : 1.0 / D.diagonal()[i];
+   }
+   return Result;
+}
+
+RealDiagonalOperator
+InvertDiagonal(RealDiagonalOperator const& D, double Tol = 1E-15)
+{
+   RealDiagonalOperator Result(D.Basis1(), D.Basis2());
+   for (unsigned i = 0; i < D.Basis1().size(); ++i)
+   {
+      Result(i,i) = InvertDiagonal(D(i,i), Tol);
+   }
+   return Result;
+}
+
+//
+// Input is A, B, Lambda
+// A,B are in left-canonical form
+// Lambda01 Gamma1 Lambda12 Gamma2 Lambda23
+// A = Lambda01 Gamma1
+// B = Lambda12 Gamma2
+// Lambda = Lambda23
+//
+// On exit, A and B are in left canonical form,
+// final Lambda' = Lambda12
+//
+
+TruncationInfo
+DoTEBD(StateComponent& A, StateComponent& B, RealDiagonalOperator& Lambda,
+       SimpleOperator const& U, StatesInfo const& SInfo)
+{
+   // simple algorithm with matrix inversion
+   RealDiagonalOperator LambdaSave = Lambda;
+   StateComponent C = local_tensor_prod(A,B);
+   StateComponent Cu = local_prod(U, C);
+   StateComponent X = Cu * Lambda;
+   AMatSVD SL(X, Tensor::ProductBasis<BasisList, BasisList>(A.LocalBasis(), B.LocalBasis()));
+   TruncationInfo Info;
+   AMatSVD::const_iterator Cutoff = TruncateFixTruncationError(SL.begin(), SL.end(),
+                                                               SInfo, Info);
+   SL.ConstructMatrices(SL.begin(), Cutoff, A, Lambda, B);
+   // normalize
+   Lambda *= 1.0 / norm_frob(Lambda);
+
+   StateComponent G = B * InvertDiagonal(LambdaSave, 1E-8);
+
+   B = Lambda*G;
+
+   CHECK_EQUAL(A.Basis2(), B.Basis1());
+   CHECK_EQUAL(A.Basis2(), Lambda.Basis1());
+
+   return Info;
+}
+#endif
+
 void DoEvenSlice(std::deque<StateComponent>& Psi,
                  std::deque<RealDiagonalOperator>& Lambda,
-                 std::vector<SimpleOperator> const& UEven,
+                 SimpleOperator const& UEven,
                  StatesInfo const& SInfo,
                  int Verbose)
 {
@@ -51,7 +287,7 @@ void DoEvenSlice(std::deque<StateComponent>& Psi,
    int MaxStates = 0;
    for (unsigned i = 0; i < Sz; i += 2)
    {
-      TruncationInfo Info = DoTEBD(Psi[i], Psi[i+1], Lambda[i/2], UEven[i/2], SInfo);
+      TruncationInfo Info = DoTEBD(Psi[i], Psi[i+1], Lambda[i/2], UEven, SInfo);
       if (Verbose > 0)
       {
          std::cout << "Bond=" << (i+1)
@@ -69,7 +305,7 @@ void DoEvenSlice(std::deque<StateComponent>& Psi,
 void DoOddSlice(std::deque<StateComponent>& Psi,
                 std::deque<RealDiagonalOperator>& Lambda,
                 QuantumNumber const& QShift,
-                std::vector<SimpleOperator> const& UOdd,
+                SimpleOperator const& UOdd,
                 StatesInfo const& SInfo,
                 int Verbose)
 {
@@ -85,7 +321,7 @@ void DoOddSlice(std::deque<StateComponent>& Psi,
   int MaxStates = 0;
    for (unsigned i = 0; i < Sz; i += 2)
    {
-      TruncationInfo Info = DoTEBD(Psi[i], Psi[i+1], Lambda[i/2], UOdd[i/2], SInfo);
+      TruncationInfo Info = DoTEBD(Psi[i], Psi[i+1], Lambda[i/2], UOdd, SInfo);
       if (Verbose > 0)
       {
          std::cout << "Bond=" << i
@@ -117,22 +353,23 @@ int main(int argc, char** argv)
       std::string OpStr;
       std::string InputFile;
       std::string OutputPrefix;
-      std::string HamStr;
+      std::string Operator;
+      std::string Operator2;
       int MinStates = 1;
       int States = 100000;
       double TruncCutoff = 0;
       double EigenCutoff = 1E-16;
       int OutputDigits = 0;
       int Coarsegrain = 1;
-      std::string DecompositionStr = "optimized4-11";
+      std::string DecompositionStr = "leapfrog2";
 
       prog_opt::options_description desc("Allowed options", terminal::columns());
       desc.add_options()
          ("help", "show this help message")
-         ("Hamiltonian,H", prog_opt::value(&HamStr),
-          "operator to use for the Hamiltonian (wavefunction attribute \"EvolutionHamiltonian\")")
 	 ("wavefunction,w", prog_opt::value(&InputFile), "input wavefunction")
 	 ("output,o", prog_opt::value(&OutputPrefix), "prefix for saving output files")
+	 ("operator", prog_opt::value(&Operator), "operator for the first slice ST decomposition")
+	 ("operator2", prog_opt::value(&Operator2), "operator for the second slice ST decomposition [optional]")
 	 ("timestep,t", prog_opt::value(&TimestepStr), "timestep (required)")
          ("decomposition,c", prog_opt::value(&DecompositionStr), FormatDefault("choice of decomposition", DecompositionStr).c_str())
 	 ("num-timesteps,n", prog_opt::value(&N), FormatDefault("number of timesteps to calculate", N).c_str())
@@ -214,100 +451,55 @@ int main(int argc, char** argv)
          OutputDigits = std::max(Digits(InitialTimeStr), Digits(TimestepStr));
       }
 
+      UnitCellMPO EvenOp, OddOp;
       InfiniteLattice Lattice;
-      BasicTriangularMPO HamMPO;
-
-      // get the Hamiltonian from the attributes, if it wasn't supplied.
-      // Get it from the EvolutionHamiltonian, if it exists,
-      // or from Hamiltonian
-      if (HamStr.empty())
+      std::tie(EvenOp, Lattice) = ParseUnitCellOperatorAndLattice(Operator);
+      if (Operator2.empty())
       {
-         if (PsiPtr->Attributes().count("EvolutionHamiltonian") == 0)
-         {
-            if (PsiPtr->Attributes().count("Hamiltonian") == 0)
-            {
-               std::cerr << "fatal: no Hamiltonian specified, use -H option or set wavefunction attribute EvolutionHamiltonian.\n";
-               return 1;
-            }
-            HamStr = PsiPtr->Attributes()["Hamiltonian"].as<std::string>();
-         }
-         else
-         {
-            HamStr = PsiPtr->Attributes()["EvolutionHamiltonian"].as<std::string>();
-         }
+	 OddOp = translate(EvenOp, Coarsegrain);
+      }
+      else
+      {
+	 std::tie(OddOp, Lattice) = ParseUnitCellOperatorAndLattice(Operator2);
       }
 
-      std::tie(HamMPO, Lattice) = ParseTriangularOperatorAndLattice(HamStr);
-      if (HamMPO.size() < Psi.size())
-	 HamMPO = repeat(HamMPO, Psi.size() / HamMPO.size());
+      EvenOp.ExtendToCover(2*Coarsegrain, 0);
+      OddOp.ExtendToCover(2*Coarsegrain, Coarsegrain);
 
-      int UnitCellSize = Psi.size();
-
-      // TODO: Coarse-grain, if necessary
-      if (vm.count("coarsegrain"))
+      if (EvenOp.offset() != 0 || EvenOp.size() != 2*Coarsegrain)
       {
-         PANIC("coarsegrain is not yet implemented");
+	 std::cerr << "mp-itebd: fatal: slice 1 operator not valid.\n";
+	 return 1;
       }
 
-      // Assemble the Hamiltonian into the bond terms
-      std::vector<SimpleOperator> BondH(UnitCellSize);
-      auto SplitTerms = SplitMPO(HamMPO);
-      for (int i = 0; i < SplitTerms.size(); ++i)
+      if (OddOp.offset() != Coarsegrain || OddOp.size() != 2*Coarsegrain)
       {
-         for (auto const& op : SplitTerms[i])
-         {
-            if (op.size() == 1)
-            {
-               // split single-site operators over the two bonds, if i=0 then
-               // the split wraps around the unit cell
-               SimpleRedOperator p = tensor_prod(op[0](0,0),HamMPO[i+1](0,0));
-               BondH[i] += 0.5 * p.scalar();
-               SimpleRedOperator q = tensor_prod(HamMPO[i-1](0,0), op[0](0,0));
-               BondH[(i+UnitCellSize-1)%UnitCellSize] += 0.5 * q.scalar();
-            }
-            else if (op.size() == 2)
-            {
-                  BondH[i] += coarse_grain(op).scalar();
-            }
-            else
-            {
-               throw std::runtime_error("fatal: operator has support over more than 2 sites.");
-            }
-         }
+	 std::cerr << "mp-itebd: fatal: slice 2 operator not valid.\n";
+	 return 1;
       }
 
-      std::vector<std::vector<SimpleOperator>> EvenU;
+      SimpleOperator EvenX = coarse_grain(EvenOp.MPO()).scalar();
+      SimpleOperator OddX = coarse_grain(EvenOp.MPO()).scalar();
+
+      std::vector<SimpleOperator> EvenU;
       for (auto x : decomp.a_)
       {
-         std::vector<SimpleOperator> Terms;
-         for (int i = 0; i < BondH.size(); i += 2)
-         {
-            Terms.push_back(Exponentiate(std::complex<double>(0, -Timestep * x) * BondH[i]));
-         }
-         EvenU.push_back(std::move(Terms));
+         EvenU.push_back(Exponentiate(std::complex<double>(0, -Timestep * x) * EvenX));
       }
 
-      std::vector<std::vector<SimpleOperator>> OddU;
+      std::vector<SimpleOperator> OddU;
       for (auto x : decomp.b_)
       {
-         std::vector<SimpleOperator> Terms;
-         for (int i = 1; i < BondH.size(); i += 2)
-         {
-            Terms.push_back(Exponentiate(std::complex<double>(0, -Timestep * x) * BondH[i]));
-         }
-         OddU.push_back(std::move(Terms));
+         OddU.push_back(Exponentiate(std::complex<double>(0, -Timestep * x) * OddX));
       }
 
       // If we have an odd number of terms (the usual case), then we can wrap around the last even slice
       // if we are continuing the evolution beyond the current timestep.  This is the sum of a[last] + a[0] terms
-      std::vector<SimpleOperator> EvenContinuation;
+      SimpleOperator EvenContinuation;
       if (decomp.a_.size() == decomp.b_.size()+1)
       {
          double x = decomp.a_.front() + decomp.a_.back();
-          for (int i = 0; i < BondH.size(); i += 2)
-         {
-            EvenContinuation.push_back(Exponentiate(std::complex<double>(0, -Timestep * x) * BondH[i]));
-         }
+         EvenContinuation = Exponentiate(std::complex<double>(0, -Timestep * x) * EvenX);
       }
 
       std::cout << "Using decomposition " << DecompositionStr << '\n';
