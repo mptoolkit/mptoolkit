723c54b321f56e5014e8db344816e750cb446aad
Jesse Osborne <jesse.osborne@protonmail.com>
2024-09-23 15:57:13 +1000

    Comment out annoying trace statement

--- mp-algorithms/triangular_mpo_solver_simple.cpp
+++ mp-algorithms/triangular_mpo_solver_simple.cpp
@@ -80,7 +80,7 @@ SolveSimpleMPO_Left(std::vector<MatrixPolyType>& EMat,
       // Generate the next C matrices, C(n) = sum_{j<Col} Op(j,Col) E_j(n)
       MatrixPolyType C = inject_left_mask(EMat, Psi, QShift, Op.data(), Psi, mask_column(Op, Col))[Col];
 
-      TRACE(C);
+      //TRACE(C);
 
       // Now do the classification, based on the properties of the diagonal operator
       BasicFiniteMPO Diag = Op(Col, Col);
