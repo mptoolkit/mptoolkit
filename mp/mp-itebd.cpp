8fe4dd6dd36e83eb67c3ebef463a1331184770c0
Jesse Osborne <jesse.osborne@protonmail.com>
2026-02-26 13:38:59 +0100

    Fix silly typo

--- mp/mp-itebd.cpp
+++ mp/mp-itebd.cpp
@@ -503,7 +503,7 @@ int main(int argc, char** argv)
          }
 
          // do we do a continuation?
-         Continue = Gates.EvenU.size() > Gates.OddU.size():
+         Continue = Gates.EvenU.size() > Gates.OddU.size();
 
          ++tstep;
          std::cout << "Timestep " << formatting::format_complex(tstep)
